99336be2b1cb9849ec8443882e28a5d8e9a33f02
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2023-02-16 18:53:00 -0500

    broweserrouter loads in to see yet if routing will work

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import Button from "@mui/material/Button";
 
 function App() {
   return (
-    <div className="App">
-      Trans Access Site
+    <>
       <Button variant="outlined">Hello World</Button>
-    </div>
+    </>
   );
 }
 
