08ad57db4e8f0b27199c8792e21a97be9b0896aa
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2023-03-23 19:25:16 -0400

    navbar has menu icon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,17 @@
-import { AppBar, Toolbar } from "@mui/material";
+import { Menu } from "@mui/icons-material";
+import { AppBar, IconButton, Toolbar } from "@mui/material";
 
 const Navbar = () => {
   return (
-    // AppBar implies bar at top of application while Toolbar implies
-    // list of utility/navigation links
+    //   {/* AppBar implies bar at top of application while Toolbar implies
+    // list of utility/navigation links */}
+    // TODO: Spacing below appbar
     <AppBar position="sticky">
-      <Toolbar></Toolbar>
+      <Toolbar>
+        <IconButton color="inherit" aria-label="menu">
+          <Menu />
+        </IconButton>
+      </Toolbar>
     </AppBar>
   );
 };
