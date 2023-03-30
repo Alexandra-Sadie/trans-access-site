ed53138dad816a78b0798cb24f28171bfa1b8660
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2023-03-30 19:47:31 -0400

    🧹🧹🧹

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -20,7 +20,6 @@ const Navbar = () => {
   return (
     //   {/* AppBar implies bar at top of application while Toolbar implies
     // list of utility/navigation links */}
-    // TODO: Spacing below appbar
     <AppBar position="sticky">
       <Toolbar>
         <IconButton color="inherit" aria-label="menu">
