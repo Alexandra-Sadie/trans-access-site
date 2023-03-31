bc961992eb30048b8cab29d1586aa7a4f3a2df24
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2023-03-30 22:31:18 -0400

    wrap hamburger icon and context menu in Boxc that only shows on mobile views

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import MenuIcon from "@mui/icons-material/Menu";
 import { AppBar, IconButton, Menu, Stack, Toolbar } from "@mui/material";
+import { Box } from "@mui/system";
 import { useState } from "react";
 
 import { routingObject } from "../../routing";
@@ -25,49 +26,51 @@ const Navbar = () => {
     // list of utility/navigation links
     <AppBar position="sticky">
       <Toolbar>
-        <IconButton
-          color="inherit"
-          aria-label="menu"
-          aria-controls="menu-appbar"
-          aria-haspopup="true"
-          onClick={handleOpenNavMenu}
+        <Box
+          // the below shows the hanburger menu at smaller (i.e. mobile) views and hides it at medium and above
+          sx={{
+            display: { xs: "block", md: "none" },
+          }}
         >
-          <MenuIcon />
-        </IconButton>
+          <IconButton
+            color="inherit"
+            aria-label="menu"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+          >
+            <MenuIcon />
+          </IconButton>
 
-        {/* COPY PASTE ZONE BELOW */}
-        <Menu
-          id="menu-appbar"
-          anchorEl={anchorElNav}
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "left",
-          }}
-          keepMounted
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "left",
-          }}
-          // this is true if anchorElNav has a value
-          // and false if it is null
-          open={Boolean(anchorElNav)}
-          onClose={handleCloseNavMenu}
-          // the below will be useful to hide the menu
-          // confidently on desktop view but not for now
-          // sx={{
-          //   display: { xs: "block", md: "none" },
-          // }}
-        >
-          {
-            // this returns an array of each route sub-object of routingObject
-            // these are expected to be of structure {route: string, title: string}
-            Object.values(routingObject)
-              // this takes said sub-objects and renders them as typographical links in the menu
-              .map(({ route, title }) => (
-                <HamburgerLink key={route} route={route} title={title} />
-              ))
-          }
-        </Menu>
+          {/* COPY PASTE ZONE BELOW */}
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            // this is true if anchorElNav has a value
+            // and false if it is null
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+          >
+            {
+              // this returns an array of each route sub-object of routingObject
+              // these are expected to be of structure {route: string, title: string}
+              Object.values(routingObject)
+                // this takes said sub-objects and renders them as typographical links in the menu
+                .map(({ route, title }) => (
+                  <HamburgerLink key={route} route={route} title={title} />
+                ))
+            }
+          </Menu>
+        </Box>
 
         {/* Stack is a Box/div that automatically is a flex container */}
         <Stack direction="row" spacing={2}>
