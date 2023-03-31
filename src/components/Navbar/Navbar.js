1553851ef5b8eb58d146877f464018ffe969cbcf
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2023-03-30 23:22:09 -0400

    hAMBURGERmENU MOVED TO CUSTOM COMPONENT

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { routingObject } from "../../routing";
 import HamburgerLink from "./HamburgerMenu/HamburgerLink";
 import DesktopNavLinks from "./DesktopNavLinks/DesktopNavLinks";
+import HamburgerMenu from "./HamburgerMenu/HamburgerMenu";
 
 const Navbar = () => {
   // *STATE ZONE
@@ -26,52 +27,11 @@ const Navbar = () => {
     // list of utility/navigation links
     <AppBar position="sticky">
       <Toolbar>
-        <Box
-          // the below shows the hanburger menu at smaller (i.e. mobile) views and hides it at medium and above
-          sx={{
-            display: { xs: "block", md: "none" },
-          }}
-        >
-          <IconButton
-            color="inherit"
-            aria-label="menu"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            onClick={handleOpenNavMenu}
-          >
-            <MenuIcon />
-          </IconButton>
-
-          {/* COPY PASTE ZONE BELOW */}
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorElNav}
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "left",
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "left",
-            }}
-            // this is true if anchorElNav has a value
-            // and false if it is null
-            open={Boolean(anchorElNav)}
-            onClose={handleCloseNavMenu}
-          >
-            {
-              // this returns an array of each route sub-object of routingObject
-              // these are expected to be of structure {route: string, title: string}
-              Object.values(routingObject)
-                // this takes said sub-objects and renders them as typographical links in the menu
-                .map(({ route, title }) => (
-                  <HamburgerLink key={route} route={route} title={title} />
-                ))
-            }
-          </Menu>
-        </Box>
-
+        <HamburgerMenu
+          anchorElNav={anchorElNav}
+          handleOpenNavMenu={handleOpenNavMenu}
+          handleCloseNavMenu={handleCloseNavMenu}
+        />
         <DesktopNavLinks />
       </Toolbar>
     </AppBar>
