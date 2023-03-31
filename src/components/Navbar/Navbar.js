d3c9c866743a26aed9c3e83f041860d5ca89b8f4
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2023-03-30 23:12:58 -0400

    🧹🧹🧹

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,10 @@
 import MenuIcon from "@mui/icons-material/Menu";
-import { AppBar, IconButton, Menu, Stack, Toolbar } from "@mui/material";
+import { AppBar, IconButton, Menu, Toolbar } from "@mui/material";
 import { Box } from "@mui/system";
 import { useState } from "react";
 
 import { routingObject } from "../../routing";
 import HamburgerLink from "./HamburgerMenu";
-import NavbarButton from "./DesktopNavLinks/NavbarButton";
 import DesktopNavLinks from "./DesktopNavLinks/DesktopNavLinks";
 
 const Navbar = () => {
@@ -73,26 +72,6 @@ const Navbar = () => {
           </Menu>
         </Box>
 
-        {/* Stack is a Box/div that automatically is a flex container */}
-        {/* <Stack
-          direction="row"
-          spacing={2}
-          // this hides on mobile breakpoints
-          // and grants its default display, flex, on
-          // desktop breakpoint
-          // note: I THINK that its default display is flex
-          sx={{ display: { xs: "none", md: "flex" } }}
-        >
-          {
-            // this returns an array of each route sub-object of routingObject
-            // these are expected to be of structure {route: string, title: string}
-            Object.values(routingObject)
-              // this takes said sub-objects and renders them as buttons
-              .map(({ route, title }) => (
-                <NavbarButton key={route} route={route} title={title} />
-              ))
-          }
-        </Stack> */}
         <DesktopNavLinks />
       </Toolbar>
     </AppBar>
