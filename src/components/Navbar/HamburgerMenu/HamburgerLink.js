d5d3433d93f75b1d174e6bc2ed647753872b8ce5
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2023-03-30 23:37:55 -0400

    wrap menuitem in link to capture all clicks

--- src/components/Navbar/HamburgerMenu/HamburgerLink.js
+++ src/components/Navbar/HamburgerMenu/HamburgerLink.js
@@ -3,12 +3,12 @@ import { Link, MenuItem, Typography } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 
 const HamburgerLink = ({ route, title }) => (
-  <MenuItem>
-    {/* i am winging that this Link>Typography the most semantic and accessible approach but it seems right */}
-    <Link sx={{ textDecoration: "none" }} to={route} component={RouterLink}>
+  //i am winging that this Link>MenuItem>Typography the most semantic and accessible approach but it seems right
+  <Link sx={{ textDecoration: "none" }} to={route} component={RouterLink}>
+    <MenuItem>
       <Typography textAlign="center">{title}</Typography>
-    </Link>
-  </MenuItem>
+    </MenuItem>
+  </Link>
 );
 
 export default HamburgerLink;
