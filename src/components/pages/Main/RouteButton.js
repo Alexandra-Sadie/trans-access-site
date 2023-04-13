1dac76cf735756aefbb34c820d47477b3425171f
s-avery <s_avery@riseup.net>
2023-04-13 19:44:27 -0400

    main & routeButton todo comments

--- src/components/pages/Main/RouteButton.js
+++ src/components/pages/Main/RouteButton.js
@@ -1,12 +1,16 @@
+// !IMPORT ZONE
 import { Link as RouterLink } from "react-router-dom";
 import { Button } from "@mui/material";
 
 const RouteButton = ({ route, title }) => {
   return (
+    // TODO link to MUI button
     <Button
       variant="contained"
       sx={{ minWidth: "200px" }}
+      // TODO see if these need template literal or can just be string lol
       to={`${route}`}
+      // TODO explain what this routerlink means
       component={RouterLink}
     >
       {`${title}`}
