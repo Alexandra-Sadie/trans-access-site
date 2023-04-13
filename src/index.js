70e6d31ba6d79a8ca6b9f79afdb5b87a171e9b4d
s-avery <s_avery@riseup.net>
2023-04-13 19:27:47 -0400

    index.js todo comments

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 // standard requisite imports
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
 import CssBaseline from "@mui/material/CssBaseline";
 // fonts
+// TODO explain this - theyre never called etc
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -13,6 +13,7 @@ import "@fontsource/roboto/700.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { routingObject } from "./routing";
 // our components
+import App from "./components/App";
 import Main from "./components/pages/Main/Main";
 import Intervention from "./components/pages/Intervention";
 import Therapist from "./components/pages/Therapist";
@@ -22,26 +23,39 @@ import Doctor from "./components/pages/Doctor";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {/* // TODO explain */}
     <CssBaseline />
+    {/* //TODO explain browserrouter -- can link to documentation */}
     <BrowserRouter>
       <Routes>
         {/* Having all routes be sub-routes of App allows us to use standard components (esp. Navbar) in app
-        and have subroutes be visible through the Outlet compoent - see App.js */}
+        and have subroutes be visible through the Outlet compoent - see App.js 
+        // TODO clarify 
+        */}
         <Route path="/" element={<App />}>
-          {/* using path="/" in the sub-route makes home fall through to this */}
+          {/* using path="/" in the sub-route makes home fall through to this
+          // TODO clarify this also lol
+           */}
+
           <Route path={routingObject.main.route} element={<Main />} />
+
           <Route
             path={routingObject.intervention.route}
             element={<Intervention />}
           />
+
           <Route path={routingObject.therapist.route} element={<Therapist />} />
+
           <Route
             path={routingObject.name_change.route}
             element={<NameChange />}
           />
+
           <Route path={routingObject.doctor.route} element={<Doctor />} />
         </Route>
-        {/* TODO: either make this properly fall through to full home page OR make custom error page 
+
+        {/* 
+        // TODO: either make this properly fall through to full home page OR make custom error page 
         for now, people will see the toolbar with links to leave*/}
         <Route path="*" element=<App /> />
       </Routes>
