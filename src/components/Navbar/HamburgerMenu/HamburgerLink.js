fb4e4fd60bc0cfc4faaf7f1f73043b4c6789db06
s-avery <s_avery@riseup.net>
2023-04-13 20:07:00 -0400

    hamburgerlink/menu todo comments

--- src/components/Navbar/HamburgerMenu/HamburgerLink.js
+++ src/components/Navbar/HamburgerMenu/HamburgerLink.js
@@ -1,11 +1,19 @@
+// !IMPORT ZONE
 import { Link, MenuItem, Typography } from "@mui/material";
-
 import { Link as RouterLink } from "react-router-dom";
 
 const HamburgerLink = ({ route, title }) => (
   //i am winging that this Link>MenuItem>Typography the most semantic and accessible approach but it seems right
-  <Link sx={{ textDecoration: "none" }} to={route} component={RouterLink}>
+  // TODO improve^
+  <Link
+    sx={{ textDecoration: "none" }}
+    to={route}
+    // TODO explain routerlink
+    component={RouterLink}
+  >
+    {/* // TODO menuitem docs link */}
     <MenuItem>
+    {/* // TODO typography docs link */}
       <Typography textAlign="center">{title}</Typography>
     </MenuItem>
   </Link>
--- src/components/Navbar/HamburgerMenu/HamburgerMenu.js
+++ src/components/Navbar/HamburgerMenu/HamburgerMenu.js
@@ -1,34 +1,41 @@
+// !IMPORT ZONE
 import MenuIcon from "@mui/icons-material/Menu";
-
 import { Box, IconButton, Menu } from "@mui/material";
 import { routingObject } from "../../../routing";
 import HamburgerLink from "./HamburgerLink";
 
 const HamburgerMenu = ({
+  // TODO why these are being passed
   anchorElNav,
   handleOpenNavMenu,
   handleCloseNavMenu,
 }) => {
   return (
     <Box
-      // the below shows the hanburger menu at smaller (i.e. mobile) views and hides it at medium and above
+      // the below shows the hamburger menu at smaller (i.e. mobile) views and hides it at medium and above
       sx={{
         display: { xs: "block", md: "none" },
       }}
     >
       <IconButton
+        // TODO link to docs
         color="inherit"
         aria-label="menu"
         aria-controls="menu-appbar"
         aria-haspopup="true"
         onClick={handleOpenNavMenu}
       >
+        {/* //TODO link to docs for menuicon
+        // ? mb rename? to be more contextually self documenting  */}
         <MenuIcon />
       </IconButton>
 
-      {/* COPY PASTE ZONE BELOW todo from where*/}
+      {/* COPY PASTE ZONE BELOW 
+      // todo pasted from where, note we modified it (how?)*/}
       <Menu
+        // TODO reevaluate if we keep this ID???
         id="menu-appbar"
+        // TODO explain anchorEl, anchorOrigin, keepMounted
         anchorEl={anchorElNav}
         anchorOrigin={{
           vertical: "bottom",
@@ -41,6 +48,7 @@ const HamburgerMenu = ({
         }}
         // this is true if anchorElNav has a value
         // and false if it is null
+        // TODO improve^
         open={Boolean(anchorElNav)}
         onClose={handleCloseNavMenu}
       >
