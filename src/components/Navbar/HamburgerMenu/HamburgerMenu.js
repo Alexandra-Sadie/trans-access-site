1b0222a1732dca23bc44639517ae6b8a079029ad
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2023-04-20 18:51:55 -0400

    parts of HamburgerMenu not related to open-close state commented

--- src/components/Navbar/HamburgerMenu/HamburgerMenu.js
+++ src/components/Navbar/HamburgerMenu/HamburgerMenu.js
@@ -1,5 +1,5 @@
 // !IMPORT ZONE
-import { Menu as HamburgerIcon } from "@mui/icons-material";
+import HamburgerIcon from "@mui/icons-material/Menu";
 import { Box, IconButton, Menu } from "@mui/material";
 import { routingObject } from "../../../routing";
 import HamburgerLink from "./HamburgerLink";
@@ -24,20 +24,22 @@ const HamburgerMenu = ({
       <IconButton
         color="inherit"
         // TODO ACCESIBILITY - determine if these aria notes are correct - they are taken copy-paste from MUI docs example
+        // in theory these are for screen readers and other accesibility devices
         aria-label="menu"
         aria-controls="menu-appbar"
         aria-haspopup="true"
         onClick={handleOpenNavMenu}
       >
-        {/* //TODO link to docs for menuicon
-        // ? mb rename? to be more contextually self documenting  */}
         <HamburgerIcon />
       </IconButton>
 
-      {/* COPY PASTE ZONE BELOW 
-      // todo pasted from where, note we modified it (how?)*/}
+      {/* Menu is an MUI component to generate a temporary menu - in this case a floating dropdown menu when the user clicks the HamburgerIcon above.
+
+      docs: https://mui.com/material-ui/react-menu/
+      
+      this code is partially copied from this example of a responsive menu in the MUI docs https://mui.com/material-ui/api/icon-button/ but we have altered it since then   */}
       <Menu
-        // TODO reevaluate if we keep this ID???
+        // accessibility: this id is neccesary for aria-controls="menu-appbar" in the IconButton above
         id="menu-appbar"
         // TODO explain anchorEl, anchorOrigin, keepMounted
         anchorEl={anchorElNav}
