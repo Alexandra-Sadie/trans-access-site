1f1c492f2cd929de027e853cb63c65d3dfa03953
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2023-04-20 18:14:34 -0400

    copied button comments from Menu/RouteButton.js to Navbar/NavbarButton.js

--- src/components/Navbar/DesktopNavLinks/NavbarButton.js
+++ src/components/Navbar/DesktopNavLinks/NavbarButton.js
@@ -1,17 +1,22 @@
 // !IMPORT ZONE
 import { Button } from "@mui/material";
+// TODO extract to readme
+// Link imported from "react-router-dom" is a React Router component that acts as an anchor link (wow!) to other routes (pages). HOWEVER because MUI ALSO has a component called Link, used to style anchor text, we import it as RouterLink across the entire project to avoid namespace confusion. This is MUI recommended practice.
+// React Router Link docs: https://reactrouter.com/en/main/components/link
+// MUI routing/link docs: https://mui.com/material-ui/guides/routing/
 import { Link as RouterLink } from "react-router-dom";
 
 // ? should we make a generic button component w variants?
 
 const NavbarButton = ({ route, title }) => {
   return (
-    // TODO link to button docs cuz why not
+    // minor note, this is the MUI-specific Button component
+    // docs: https://mui.com/material-ui/react-button/
     <Button
       variant="text"
       sx={{ color: "#fff" }}
       to={`${route}`}
-      // TODO clarify routerlink
+      // see above on why this is RouterLink
       component={RouterLink}
     >
       {`${title}`}
