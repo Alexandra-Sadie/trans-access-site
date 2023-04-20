a13f2967f3afc47d08cf507a6a94cbdf861c6953
s-avery <s_avery@riseup.net>
2023-04-20 17:04:32 -0400

    react router/routes comments, removed unnecessary path="/"

--- src/index.js
+++ src/index.js
@@ -27,18 +27,30 @@ root.render(
     docs: https://mui.com/material-ui/react-css-baseline/
     */}
     <CssBaseline />
-    {/* //TODO explain browserrouter -- can link to documentation */}
+    {/* 
+    BrowserRouter is a part of React Router - since this is the first point you're seeig React Router, this comment briefly explains what React Router is.
+    
+    React itself renders a "Single Page App", using only one html page behind the scenes, and using javascript to change what that page looks like. See public/index.html, or README.md to learn more.
+    
+    This is powerful but takes away the ability for the user to enter URLs to access specific pages of our app, since there is only one webpage loaded.
+    
+    React Router gives us BACK this URL functionality, by intercepting the load of a new page when the user enters a sub-url on our domain, and sending that data to React instead, to load page and component views itself.
+    
+    See especially this page of the React Router docs: https://reactrouter.com/en/6.10.0/start/concepts
+    
+    BrowserRouter is the higher-order component that lets us define all the url bar routes our app will accept, defined below as children of BrowserRouter.
+    
+    Component documentation here: https://reactrouter.com/en/6.10.0/router-components/browser-router
+    //TODO extract some of this to readme.md
+ */}
     <BrowserRouter>
       <Routes>
-        {/* Having all routes be sub-routes of App allows us to use standard components (esp. Navbar) in app
-        and have subroutes be visible through the Outlet compoent - see App.js 
-        // TODO clarify 
+        {/* 
+        We've structured our routes (AKA our pages) as nested sub-routes "inside" of App.js. This lets us use the React Router Outlet feature to load universal content in App.js which will be visible on every sub-route (AKA every page). We use this to make the Navbar load identically everywhere.
+        See comments on app.js for further info. 
+        docs: https://reactrouter.com/en/6.10.0/components/outlet
         */}
-        <Route path="/" element={<App />}>
-          {/* using path="/" in the sub-route makes home fall through to this
-          // TODO clarify this also lol
-           */}
-
+        <Route element={<App />}>
           <Route path={routingObject.main.route} element={<Main />} />
 
           <Route
@@ -59,7 +71,7 @@ root.render(
         {/* 
         // TODO: either make this properly fall through to full home page OR make custom error page 
         for now, people will see the toolbar with links to leave*/}
-        <Route path="*" element=<App /> />
+        <Route path="*" element={<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
