4847dd09b3f8989798db8673d630f7a3a21a94f1
s-avery <s_avery@riseup.net>
2023-04-20 17:49:36 -0400

    Navbar.js comments

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,31 +5,35 @@ import { useState } from "react";
 import DesktopNavLinks from "./DesktopNavLinks/DesktopNavLinks";
 import HamburgerMenu from "./HamburgerMenu/HamburgerMenu";
 
-// TODO explain the navbar philosophy
 const Navbar = () => {
   // *STATE ZONE
-  // TODO what this state does
+  // we handle the hamburger dropdown menu by anchoring the menu to the hamburger icon when the hamburger is clicked on. (when the menu has no anchor it doesn't appear)
+  // this state is used to monitor what html element the menu is anchoring to, if any (should only ever be the burger)
   const [anchorElNav, setAnchorElNav] = useState(null);
 
   // *FUNCTION ZONE
-  // TODO how this works to actually open it
+  // this function expects to receive a click event & is called in the onClick of the hamburger icon
+  // when this function runs, it will set the anchor of the dropdown menu to the clicked element (the burger)
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
 
-  // TODO likewise to close
+  // this fn is called when the dropdown menu is closed, using MUI's onClose trigger that it provides attached to the Menu component
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
 
   // *RETURN
   return (
-    //   AppBar implies bar at top of application while Toolbar implies
-    // list of utility/navigation links
-    // todo link to stackoverflow post about this -- explain position sticky also
+    // AppBar implies a bar at top of application while Toolbar implies a list of utility/navigation links -- we have a Toolbar nested INSIDE an AppBar bc it's a list of links placed at the top of the app :)
+    // Stack Overflow comment explaining this: https://stackoverflow.com/a/61145181
+    // the position="sticky" forces the AppBar to take space in the pageflow so it doesn't float over content (but still stays on top when you scroll)
+
+    // MUI AppBar docs: https://mui.com/material-ui/react-app-bar/
+    // MUI Toolbar docs: https://mui.com/material-ui/api/toolbar/
     <AppBar position="sticky">
       <Toolbar>
-        {/* //TODO mb put the sx breakpoints css here in these elements to make it self-commenting */}
+        {/* // ? mb put the sx breakpoints css here in these elements to make it self-commenting */}
         {/* this is visible on mobile breakpoints */}
         <HamburgerMenu
           anchorElNav={anchorElNav}
