de8d40b263208cf6b4ab2090d908707519cc688a
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2023-04-20 18:31:50 -0400

    HamburgerLink commented

--- src/components/Navbar/HamburgerMenu/HamburgerLink.js
+++ src/components/Navbar/HamburgerMenu/HamburgerLink.js
@@ -1,19 +1,30 @@
 // !IMPORT ZONE
 import { Link, MenuItem, Typography } from "@mui/material";
+// TODO extract to readme
+// Link imported from "react-router-dom" is a React Router component that acts as an anchor link (wow!) to other routes (pages). HOWEVER because MUI ALSO has a component called Link, used to style anchor text, we import it as RouterLink across the entire project to avoid namespace confusion.
+// For example, in this file, we import Link from MUI above and need to import React Router's Link under this different name.
+// This is MUI recommended practice.
+// React Router Link docs: https://reactrouter.com/en/main/components/link
+// MUI routing/link docs: https://mui.com/material-ui/guides/routing/
 import { Link as RouterLink } from "react-router-dom";
 
 const HamburgerLink = ({ route, title }) => (
-  //i am winging that this Link>MenuItem>Typography the most semantic and accessible approach but it seems right
-  // TODO improve^
+  //TODO Link>MenuItem>Typography seems to be the most semantic and accessible way to make a MenuItem into a link, but we should determine if this is truly the correct accessibility approach
   <Link
     sx={{ textDecoration: "none" }}
     to={route}
-    // TODO explain routerlink
+    // see above on why this is RouterLink called inside an MUI Link
     component={RouterLink}
   >
-    {/* // TODO menuitem docs link */}
+    {/* This is a MUI component, designed to be a child of a higher-up Menu component - note that the Menu component is not in this file, but in HamburgerMenu.js, which calls this HamburgerLink component, making this MenuItem a valid child of that Menu
+    
+    MenuItem mostly creates nice spacing around items in a MUI menu
+    docs: https://mui.com/material-ui/api/menu-item/
+    */}
     <MenuItem>
-    {/* // TODO typography docs link */}
+      {/* Typography is a MUI component for all text elements in a page - the default is a <p> tag, which this element is
+    docs: https://mui.com/material-ui/api/typography/
+    */}
       <Typography textAlign="center">{title}</Typography>
     </MenuItem>
   </Link>
