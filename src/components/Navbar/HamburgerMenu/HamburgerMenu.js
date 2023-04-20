e91a53fde72e9f4390b8c2a938bf6afdcc3b9e06
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2023-04-20 18:54:42 -0400

    moved state management from NavBar to HamburgerMenu

--- src/components/Navbar/HamburgerMenu/HamburgerMenu.js
+++ src/components/Navbar/HamburgerMenu/HamburgerMenu.js
@@ -3,13 +3,27 @@ import HamburgerIcon from "@mui/icons-material/Menu";
 import { Box, IconButton, Menu } from "@mui/material";
 import { routingObject } from "../../../routing";
 import HamburgerLink from "./HamburgerLink";
+import { useState } from "react";
 
-const HamburgerMenu = ({
-  // TODO why these are being passed
-  anchorElNav,
-  handleOpenNavMenu,
-  handleCloseNavMenu,
-}) => {
+const HamburgerMenu = () => {
+  // *STATE ZONE
+  // we handle the hamburger dropdown menu by anchoring the menu to the hamburger icon when the hamburger is clicked on. (when the menu has no anchor it doesn't appear)
+  // this state is used to monitor what html element the menu is anchoring to, if any (should only ever be the burger)
+  const [anchorElNav, setAnchorElNav] = useState(null);
+
+  // *FUNCTION ZONE
+  // this function expects to receive a click event & is called in the onClick of the hamburger icon
+  // when this function runs, it will set the anchor of the dropdown menu to the clicked element (the burger)
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  // this fn is called when the dropdown menu is closed, using MUI's onClose trigger that it provides attached to the Menu component
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  // *RETURN
   return (
     <Box
       // the below shows the hamburger menu at smaller (i.e. mobile) views and hides it at medium and above
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,29 +1,10 @@
 // !IMPORT ZONE
 import { AppBar, Toolbar } from "@mui/material";
-import { useState } from "react";
 // our components
 import DesktopNavLinks from "./DesktopNavLinks/DesktopNavLinks";
 import HamburgerMenu from "./HamburgerMenu/HamburgerMenu";
 
 const Navbar = () => {
-  // *STATE ZONE
-  // we handle the hamburger dropdown menu by anchoring the menu to the hamburger icon when the hamburger is clicked on. (when the menu has no anchor it doesn't appear)
-  // this state is used to monitor what html element the menu is anchoring to, if any (should only ever be the burger)
-  const [anchorElNav, setAnchorElNav] = useState(null);
-
-  // *FUNCTION ZONE
-  // this function expects to receive a click event & is called in the onClick of the hamburger icon
-  // when this function runs, it will set the anchor of the dropdown menu to the clicked element (the burger)
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  // this fn is called when the dropdown menu is closed, using MUI's onClose trigger that it provides attached to the Menu component
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  // *RETURN
   return (
     // AppBar implies a bar at top of application while Toolbar implies a list of utility/navigation links -- we have a Toolbar nested INSIDE an AppBar bc it's a list of links placed at the top of the app :)
     // Stack Overflow comment explaining this: https://stackoverflow.com/a/61145181
@@ -35,11 +16,7 @@ const Navbar = () => {
       <Toolbar>
         {/* // ? mb put the sx breakpoints css here in these elements to make it self-commenting */}
         {/* this is visible on mobile breakpoints */}
-        <HamburgerMenu
-          anchorElNav={anchorElNav}
-          handleOpenNavMenu={handleOpenNavMenu}
-          handleCloseNavMenu={handleCloseNavMenu}
-        />
+        <HamburgerMenu />
         {/* this is visible on desktop breakpoint */}
         <DesktopNavLinks />
       </Toolbar>
