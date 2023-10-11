6e303d08fe34b7fe3b4a0964085ac339f9b85b3d
s-avery <s_avery@riseup.net>
2023-10-11 19:52:04 -0400

    login password and button

--- src/components/pages/LoginSignup/LoginSignup.js
+++ src/components/pages/LoginSignup/LoginSignup.js
@@ -1,4 +1,4 @@
-import { Card, Stack, Typography, Box, TextField } from "@mui/material";
+import { Card, Stack, Typography, TextField, Button } from "@mui/material";
 
 const LoginSignup = () => {
   return (
@@ -15,20 +15,41 @@ const LoginSignup = () => {
       justifyContent={"center"}
       alignItems={"center"}
     >
+      {/* //*Login */}
       <Card>
         {/* //TODO we may want this to not be component and/or variant h4 for semantics */}
         <Typography variant="h4">Login</Typography>
 
-        <Box component="form" noValidate>
+        <Stack component="form" noValidate>
+          {/* email */}
           <TextField
             margin="normal"
             required
             id="email"
             label="Email Address"
             name="email"
+            //type="email" adds validation parameters and mobile keyboard customized for email input -- https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#Form_%3Cinput%3E_types
+            type="email"
             autoComplete="email"
           ></TextField>
-        </Box>
+
+          {/* password */}
+          <TextField
+            margin="normal"
+            required
+            name="password"
+            label="Password"
+            // TODO can we add a button to make it visible?
+            // this censors the text as you type it -- https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#Form_%3Cinput%3E_types
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          ></TextField>
+
+          <Button type="submit" variant="contained">
+            Login
+          </Button>
+        </Stack>
       </Card>
       <Card>
         <Typography variant="h4">Sign Up</Typography>
