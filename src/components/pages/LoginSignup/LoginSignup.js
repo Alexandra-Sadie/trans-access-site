e7fd69d6eeea7f0726bee7175f267f2ce88afd0e
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2023-10-11 19:00:56 -0400

    added cards, doubly centered the stack, added top padding that varies by breakpoint

--- src/components/pages/LoginSignup/LoginSignup.js
+++ src/components/pages/LoginSignup/LoginSignup.js
@@ -1,14 +1,23 @@
-import { Stack } from "@mui/material";
+import { Card, Stack } from "@mui/material";
 
 const LoginSignup = () => {
   return (
+    // Stack is a flex-container for one-directional groupings
+    // here we are grouping a Login card and a singup card
     <Stack
+      // pt is padding-top; for now we add a little more padding to the desktop view
+      sx={{ pt: { xs: 6, md: 12 } }}
       // xs is mobile breakpoint, display as a column, md is desktop breakpoint, display side by side
       direction={{ xs: "column", md: "row" }}
       // this spacing property uses the spacing unit set in the MUI theme
       // docs: https://mui.com/material-ui/customization/spacing/
       spacing={2}
-    ></Stack>
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <Card>test1</Card>
+      <Card>test2</Card>
+    </Stack>
   );
 };
 
