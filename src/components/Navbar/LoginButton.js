49bc209786465efc24969a0e1c51dd98b66fb626
s-avery <s_avery@riseup.net>
2023-10-19 17:47:45 -0400

    login/ my account button in nav

--- src/components/Navbar/LoginButton.js
+++ src/components/Navbar/LoginButton.js
@@ -1,20 +1,48 @@
 import { useContext } from "react";
 import { UserLoggedInContext } from "../../providers/UserLoggedInProvider";
-import { Button } from "@mui/material";
+import { Button, Stack } from "@mui/material";
 
 const LoginButton = () => {
   const { isUserLoggedIn, logIn, logOut } = useContext(UserLoggedInContext);
   return (
-    <Button
-      variant="contained"
-      sx={{
-        color: "#fff",
-        fontSize: { sm: "small", md: "medium" },
-      }}
-      onClick={isUserLoggedIn ? logOut : logIn}
-    >
-      {isUserLoggedIn ? "Log Out" : "Log In"}
-    </Button>
+    <Stack direction="row" spacing={2}>
+      {isUserLoggedIn ? (
+        <>
+          <Button
+            variant="contained"
+            sx={{
+              color: "#fff",
+              fontSize: { sm: "small", md: "medium" },
+            }}
+          >
+            My Account
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              color: "#fff",
+              fontSize: { sm: "small", md: "medium" },
+            }}
+            onClick={logOut}
+          >
+            Log Out
+          </Button>
+        </>
+      ) : (
+        <>
+          <Button
+            variant="contained"
+            sx={{
+              color: "#fff",
+              fontSize: { sm: "small", md: "medium" },
+            }}
+            onClick={logIn}
+          >
+            Log In
+          </Button>
+        </>
+      )}
+    </Stack>
   );
 };
 export default LoginButton;
