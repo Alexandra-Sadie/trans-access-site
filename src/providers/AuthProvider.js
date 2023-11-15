c5493fad31f9dc90df7ceb101871f51f6b3d0ea9
s-avery <s_avery@riseup.net>
2023-11-15 18:13:38 -0500

    french create user fn

--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -59,6 +59,17 @@ const AuthProvider = ({ children }) => {
       .catch((err) => console.error(err));
   };
 
+  const createNewUserFrench = (signupEmail, signupPassword) => {
+    createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
+      // TODO: decide if we really want to do anything here
+      .then((userCredential) => {
+        console.log(userCredential);
+        auth.languageCode = "fr";
+        sendEmailVerification(auth.currentUser);
+      })
+      .catch((err) => console.error(err));
+  };
+
   return (
     // passing down our relevant state and associated functions in an anonymous object that children can pull from as needed using useContext
     <AuthContext.Provider value={{ user, logIn, logOut, createNewUserEnglish }}>
