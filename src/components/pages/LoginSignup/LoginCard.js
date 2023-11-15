2c98f149f320f3e9a7838bd68f716a89a1708d08
s-avery <s_avery@riseup.net>
2023-11-15 18:35:22 -0500

    moved login to form onsubmit

--- src/components/pages/LoginSignup/LoginCard.js
+++ src/components/pages/LoginSignup/LoginCard.js
@@ -21,6 +21,12 @@ const LoginCard = () => {
         component="form"
         // TODO do we want novalidate? with it gone, it checks for valid email which is handy, but do we need it so html validation doesnt fuck with react?
         noValidate
+        onSubmit={(e) => {
+          e.preventDefault();
+          logIn(currentLoginEmail, currentLoginPassword);
+          // this timeout is to look nice, remove for final production
+          navigate(routingObject.main.route);
+        }}
       >
         {/* email */}
         <TextField
@@ -54,18 +60,7 @@ const LoginCard = () => {
           }}
         ></TextField>
 
-        <Button
-          type="submit"
-          variant="contained"
-          onClick={(e) => {
-            e.preventDefault();
-            logIn(currentLoginEmail, currentLoginPassword);
-            // this timeout is to look nice, remove for final production
-            setTimeout(() => {
-              navigate(routingObject.main.route);
-            }, 300);
-          }}
-        >
+        <Button type="submit" variant="contained">
           Log In
         </Button>
       </Stack>
