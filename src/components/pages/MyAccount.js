3e8625860281f96474726cd52efeca082caec18e
s-avery <s_avery@riseup.net>
2023-11-15 18:32:15 -0500

    tracking user email verification

--- src/components/pages/MyAccount.js
+++ src/components/pages/MyAccount.js
@@ -1,9 +1,23 @@
 import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
+import { auth } from "../../firebase";
 
 const MyAccount = () => {
   const { user } = useContext(AuthContext);
 
-  return <div>{user ? JSON.stringify(user) : "No current user"}</div>;
+  return (
+    <div>
+      {user ? (
+        <>
+          <p>current user: {user.email}</p>
+          <p>is this user email-verified? {`${user.emailVerified}`}</p>
+          <p>full user object:</p>
+          <p>{JSON.stringify(user)}</p>
+        </>
+      ) : (
+        "No current user"
+      )}
+    </div>
+  );
 };
 export default MyAccount;
