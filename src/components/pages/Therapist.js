7f6dbe44667e22cafc148dee4f73d20de7fd7e2a
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2023-11-23 22:36:03 -0500

    A Therpaist loads in

--- src/components/pages/Therapist.js
+++ src/components/pages/Therapist.js
@@ -5,9 +5,18 @@ const Therapist = () => {
   const cards = useContext(CardsContext);
   return (
     <>
-      {cards.therapist.map((therapistCard) => (
-        <div key={therapistCard.uid}>{JSON.stringify(therapistCard)}</div>
-      ))}
+      {cards.therapist.map(
+        ({ uid, name, profession, location, waitTime, bio }) => (
+          <div key={uid}>
+            <h1>{name}</h1>
+            <h2>{profession}</h2>
+            <h3>{location}</h3>
+            <p>{`Wait Time: ${waitTime} weeks`}</p>
+            <p>{bio}</p>
+            <br />
+          </div>
+        )
+      )}
     </>
   );
 };
