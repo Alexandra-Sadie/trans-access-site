5ce5c1d771a186cc3e465e9dd5409af62be49bc8
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2023-11-23 23:32:29 -0500

    comments explaining mock server logic

--- src/components/pages/Therapist.js
+++ src/components/pages/Therapist.js
@@ -8,15 +8,32 @@ const Therapist = () => {
     useContext(CardsContext);
 
   // this pulls the pathname off of the route
+  // i.e. "therapist" for the therapist page etc
   // TODO: explain this better, maybe find a more elegant way with router??
   const cardSubset = useLocation().pathname.slice(1);
 
-  useEffect(() => {
-    requestNewCards(cardSubset);
-    console.count("inside " + cardSubset + " useEffect");
+  // on mount, currentCards is empty array
+  // THEN this fires, and requests a specific serviceType array from CardsProvider
+  // based on the service type in the route of the page
+  // that array then populates into currentCards
+  //
+  // this is to mock the logic of sending a server request on load
+  // and then receiving the cards from that request
+  // long term useEffect may be wrong herre but i think is ok for the lifetime of this mock
+  useEffect(
+    () => {
+      requestNewCards(cardSubset);
+      // this is to double check we're not creating an infinite loop
+      console.count("inside " + cardSubset + " useEffect");
 
-    return resetCards;
-  }, [requestNewCards, cardSubset, resetCards]);
+      return resetCards;
+    },
+    // eslint requires these but they are static
+    // useEffect may be hacky here
+    // TODO: read react docs on using something other than useEffect
+    // however this logic will be different when the real db is up anyway
+    [requestNewCards, cardSubset, resetCards]
+  );
 
   return (
     <>
