26d704ff33f45267a052fa6bd4b5f34ded79ce63
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2023-11-23 23:17:53 -0500

    logic to request specific cards

--- src/components/pages/Therapist.js
+++ src/components/pages/Therapist.js
@@ -1,9 +1,9 @@
 import { useContext } from "react";
 import { CardsContext } from "../../providers/CardsProvider";
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Therapist = () => {
-  const cards = useContext(CardsContext);
+  const { currentCards } = useContext(CardsContext);
   // this pulls the pathname off of the route
   // TODO: explain this better, maybe find a more elegant way with router??
   const cardSubset = useLocation().pathname.slice(1);
@@ -12,18 +12,20 @@ const Therapist = () => {
 
   return (
     <>
-      {cards.therapist.map(
-        ({ uid, name, profession, location, waitTime, bio }) => (
-          <div key={uid}>
-            <h1>{name}</h1>
-            <h2>{profession}</h2>
-            <h3>{location}</h3>
-            <p>{`Wait Time: ${waitTime} weeks`}</p>
-            <p>{bio}</p>
-            <br />
-          </div>
-        )
-      )}
+      {currentCards
+        ? currentCards.map(
+            ({ uid, name, profession, location, waitTime, bio }) => (
+              <div key={uid}>
+                <h1>{name}</h1>
+                <h2>{profession}</h2>
+                <h3>{location}</h3>
+                <p>{`Wait Time: ${waitTime} weeks`}</p>
+                <p>{bio}</p>
+                <br />
+              </div>
+            )
+          )
+        : null}
     </>
   );
 };
--- src/providers/CardsProvider.js
+++ src/providers/CardsProvider.js
@@ -1,7 +1,9 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const CardsContext = createContext({});
 
+// this is a dummy list of cards, this will be server data later in development
+// the top level properties match our main provider routes "therapist, doctor, etc"
 const cards = {
   therapist: [
     {
@@ -26,8 +28,22 @@ const cards = {
 };
 
 const CardsProvider = ({ children }) => {
+  // the cardsList that is active in the app currently - defaults to empty array
+  const [currentCards, setCurrentCards] = useState([]);
+
+  //this receives a serviceType as a string, the string should be one of the top-level
+  //properties of the static card object, which match our main service provider routes
+  //it then sets currentCards to the array of provider cards matching that serviceType
+  const requestNewCards = (serviceType) => setCurrentCards(cards[serviceType]);
+
+  //This simply resets currentCards to an empty array, to be used on provider card unmount (i.e. the return value of a useEffect)
+  const resetCards = () => setCurrentCards([]);
   return (
-    <CardsContext.Provider value={cards}>{children}</CardsContext.Provider>
+    <CardsContext.Provider
+      value={{ currentCards, requestNewCards, resetCards }}
+    >
+      {children}
+    </CardsContext.Provider>
   );
 };
 export default CardsProvider;
