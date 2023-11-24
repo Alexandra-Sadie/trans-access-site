819d494cca07f188181a11fb20451a80fd245486
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2023-11-23 22:49:30 -0500

    added second therapist card

--- src/providers/CardsProvider.js
+++ src/providers/CardsProvider.js
@@ -11,7 +11,16 @@ const cards = {
       location: "123 X Street, Montreal, Quebec, V6B 4A2",
       available: true,
       waitTime: 2,
-      bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus maiores minima eum laboriosam neque molestiae suscipit obcaecati reiciendis inventore soluta! Nobis corrupti esse laborum ad! Officiis alias quae autem obcaecati quos omnis, ex dolor porro reiciendis sed assumenda facilis. Repellat, voluptate, mollitia maxime ducimus qui fuga totam minima velit hic cupiditate laudantium? Quo perspiciatis consectetur culpa maxime iure? Accusamus quis architecto officiis totam repudiandae corrupti magni, eius omnis iste deleniti nostrum itaque adipisci nisi ipsa aliquam aperiam nihil repellat sapiente cupiditate? Fuga excepturi id quasi illum dolore explicabo dignissimos qui at rem aliquid magni, minus enim facilis quae placeat delectus modi soluta, cupiditate architecto. Tempore deserunt at omnis blanditiis? Neque repellat commodi dolore aliquam dicta id quidem excepturi architecto hic.",
+      bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus maiores minima eum laboriosam neque molestiae suscipit obcaecati reiciendis inventore soluta! Nobis corrupti esse laborum ad! Officiis alias quae autem obcaecati quos omnis, ex dolor porro reiciendis sed assumenda facilis. Repellat, voluptate, mollitia maxime ducimus qui fuga totam minima velit hic cupiditate laudantium?",
+    },
+    {
+      uid: "2",
+      name: "Jane Doe 2",
+      profession: "Counselor",
+      location: "225 Y Street, Montreal, Quebec, V6B 4A2",
+      available: true,
+      waitTime: 7,
+      bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident porro magni assumenda eveniet nisi doloribus? Odio dignissimos dolor officia asperiores nam dolorum labore blanditiis cum, dolores libero ea. Reprehenderit numquam voluptatibus explicabo quo ipsum nam iure unde nulla eius magnam assumenda optio sit neque, esse facere voluptas impedit itaque animi. Exercitationem, at nulla rem inventore perspiciatis error dolorem, suscipit facere natus tempora quis esse? Maiores vero laborum, voluptates qui porro corrupti dicta inventore dignissimos excepturi distinctio iure!",
     },
   ],
 };
