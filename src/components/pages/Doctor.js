eae90a427e111755772332ce5b06d9a7ad026e6b
s-avery <s_avery@riseup.net>
2023-11-28 14:08:50 -0500

    avatar or stringAvatar fn on their name

--- src/components/pages/Doctor.js
+++ src/components/pages/Doctor.js
@@ -17,6 +17,8 @@ const Doctor = () => {
       available: true,
       waitTime: 2,
       bio: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus magnam quam perspiciatis dolorum aliquam laudantium optio corrupti culpa a, fugit quas odit quo, minus similique non, voluptas libero veritatis doloribus.",
+      avatar:
+        "https://cdn.discordapp.com/attachments/920024348335689800/1178203372181008414/F_0bnEhXYAAIBY8.jpg?ex=65754a65&is=6562d565&hm=b26b3956d121645d8a725af263d48500f66b4d36cb5fdb7b37b9b086ba44a226&",
     },
     {
       uid: "2",
@@ -35,8 +37,16 @@ const Doctor = () => {
       available: true,
       waitTime: 2,
       bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quos molestias asperiores repellendus quibusdam alias voluptatum molestiae. Optio impedit incidunt veniam et libero officia placeat, corrupti sequi at. Omnis, soluta!",
+      avatar:
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupl.roob.la%2F2015%2F01%2Fdale-cooper-coffee-e1421147082530.png&f=1&nofb=1&ipt=060a6fa657b51ce6c3bdd1485a4a66da671dc9fe9d27985729ae6026c286bab6&ipo=images",
     },
   ];
+
+  const stringAvatar = (name) => {
+    return {
+      children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+    };
+  };
 
   return dummyDoctorsList.map((professional) => {
     return (
@@ -44,7 +54,13 @@ const Doctor = () => {
         <CardHeader
           title={professional.name}
           // can this be square?
-          avatar={<Avatar></Avatar>}
+          avatar={
+            professional.avatar ? (
+              <Avatar src={professional.avatar}>yes</Avatar>
+            ) : (
+              <Avatar {...stringAvatar(professional.name)} />
+            )
+          }
           subheader={professional.profession}
         ></CardHeader>
         <CardContent>
