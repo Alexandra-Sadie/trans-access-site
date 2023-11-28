876e5ba984465262a5a90f87711f6f46856a66eb
s-avery <s_avery@riseup.net>
2023-11-28 12:41:07 -0500

    created card and cardheader w/ title

--- src/components/pages/Doctor.js
+++ src/components/pages/Doctor.js
@@ -1,7 +1,26 @@
-const Doctor = () => {
-  return (
-    <div>Doctor</div>
-  )
-}
+import { Card, CardHeader } from "@mui/material";
 
-export default Doctor
+const Doctor = () => {
+  const modelDoctor = {
+    uid: "1",
+    name: "Jane Doe",
+    profession: "Therapist",
+    location: "123 X Street, Montreal, Quebec, V6B 4A2",
+    available: true,
+    waitTime: 2,
+    bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus maiores minima eum laboriosam neque molestiae suscipit obcaecati reiciendis inventore soluta! Nobis corrupti esse laborum ad! Officiis alias quae autem obcaecati quos omnis, ex dolor porro reiciendis sed assumenda facilis. Repellat, voluptate, mollitia maxime ducimus qui fuga totam minima velit hic cupiditate laudantium?",
+  };
+
+  console.log(modelDoctor);
+
+  return (
+    <div>
+      Doctor
+      <Card sx={{ maxWidth: 400 }}>
+        <CardHeader title={modelDoctor.name}></CardHeader>
+      </Card>
+    </div>
+  );
+};
+
+export default Doctor;
