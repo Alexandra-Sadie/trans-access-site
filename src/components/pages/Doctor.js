591993a63253bab5a0e29af494d6394e21a36bdd
s-avery <s_avery@riseup.net>
2023-11-28 12:56:15 -0500

    bee movie

--- src/components/pages/Doctor.js
+++ src/components/pages/Doctor.js
@@ -14,7 +14,7 @@ const Doctor = () => {
     location: "123 X Street, Montreal, Quebec, V6B 4A2",
     available: true,
     waitTime: 2,
-    bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus maiores minima eum laboriosam neque molestiae suscipit obcaecati reiciendis inventore soluta! Nobis corrupti esse laborum ad! Officiis alias quae autem obcaecati quos omnis, ex dolor porro reiciendis sed assumenda facilis. Repellat, voluptate, mollitia maxime ducimus qui fuga totam minima velit hic cupiditate laudantium?",
+    bio: "According to all known laws of aviation, there is no way that a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.",
   };
 
   console.log(modelDoctor);
@@ -34,12 +34,7 @@ const Doctor = () => {
         <CardContent>
           <Card>{modelDoctor.location}</Card>
           <Card>Wait time: {modelDoctor.waitTime} months</Card>
-          <Typography noWrap={true}>
-            According to all known laws of aviation, there is no way that a bee
-            should be able to fly. Its wings are too small to get its fat little
-            body off the ground. The bee, of course, flies anyway because bees
-            don't care what humans think is impossible.
-          </Typography>
+          <Typography noWrap={true}>{modelDoctor.bio}</Typography>
         </CardContent>
       </Card>
     </div>
