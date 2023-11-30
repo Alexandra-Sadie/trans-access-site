2a98cc8bd04fffd17d696d748585b3078b50010f
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2023-11-30 17:00:54 -0500

    chose good spacing for cards

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -6,8 +6,10 @@ const CardList = ({ cardArr }) => {
     // Stack component="ul"
     // gives us access to spacing, flex, etc properties of Stack
     // as vs. just List
-    // TODO stack overflow this maybe:?
-    <Stack component="ul" spacing={150}>
+    // ? stack overflow this maybe:?
+    // ? choose best spacing, but xs1md2 seems ok?
+    // ? maybe add padding top
+    <Stack component="ul" spacing={{ xs: 1, md: 2 }}>
       {cardArr.map((professional) => (
         <ProfessionalCard key={professional.uid} {...professional} />
       ))}
