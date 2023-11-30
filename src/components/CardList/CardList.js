93744f0480311cb6d60c99bd10458cc19a623ad2
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2023-11-29 19:48:01 -0500

    card is tidied out

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,65 +1,12 @@
-import {
-  Avatar,
-  Card,
-  CardHeader,
-  CardContent,
-  Typography,
-  List,
-  ListItem,
-} from "@mui/material";
-
-// uses .split (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split) to extract the first letter of the first word in name and the first letter of the second word in name
-// could think about updating to have a third letter etc
-const stringAvatar = (name) =>
-  name
-    .split(" ")
-    .map((nameWord) => nameWord[0])
-    .join("");
+import { List } from "@mui/material";
+import ProfessionalCard from "./ProfessionalCard";
 
 const CardList = ({ cardArr }) => {
   return (
     <List>
-      {cardArr.map((professional) => {
-        return (
-          <ListItem key={professional.uid}>
-            <Card variant="outlined" sx={{ width: 400 }}>
-              <CardHeader
-                title={professional.name}
-                titleTypographyProps={{ fontSize: 20 }}
-                // can this be square?
-                avatar={
-                  <Avatar
-                    src={professional.avatar}
-                    // if they don't have an avatar, it runs stringAvatar on their name to make the avatar their initials
-                    // this fallback will also apply if they have a listed avatar but it is somehow invalid (bc src looks for image link), but otherwise is overwritten by the image
-                    // {...stringAvatar(professional.name)}
-                  >
-                    {stringAvatar(professional.name)}
-                  </Avatar>
-                }
-                subheader={
-                  <>
-                    <Typography>{professional.profession}</Typography>
-                  </>
-                }
-              ></CardHeader>
-              <CardContent>
-                <Typography>Location: {professional.location}</Typography>
-                <Typography>
-                  Wait time: {professional.waitTime} months
-                </Typography>
-                <Typography
-                  sx={{ mt: 1 }}
-                  // noWrap is what makes it cut short with ellipsis at card limit
-                  noWrap={true}
-                >
-                  Bio: {professional.bio}
-                </Typography>
-              </CardContent>
-            </Card>
-          </ListItem>
-        );
-      })}
+      {cardArr.map((professional) => (
+        <ProfessionalCard {...professional} />
+      ))}
     </List>
   );
 };
