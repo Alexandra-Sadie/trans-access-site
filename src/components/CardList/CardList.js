64c0428702138be1376f52a9f62312b367f5022e
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2023-11-30 16:56:22 -0500

    stack ul instead of List

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,13 +1,17 @@
-import { List } from "@mui/material";
+import { Stack } from "@mui/material";
 import ProfessionalCard from "./ProfessionalCard";
 
 const CardList = ({ cardArr }) => {
   return (
-    <List>
+    // Stack component="ul"
+    // gives us access to spacing, flex, etc properties of Stack
+    // as vs. just List
+    // TODO stack overflow this maybe:?
+    <Stack component="ul" spacing={150}>
       {cardArr.map((professional) => (
         <ProfessionalCard key={professional.uid} {...professional} />
       ))}
-    </List>
+    </Stack>
   );
 };
 export default CardList;
