0358c91b4bf20580b89347466b2f0658f09c50a2
s-avery <s_avery@riseup.net>
2024-01-12 14:53:11 -0500

    bg & text color light mode

--- src/providers/CustomThemesProvider.js
+++ src/providers/CustomThemesProvider.js
@@ -15,11 +15,18 @@ const CustomThemesProvider = ({ children }) => {
 
   // TODO presumably we will rename this lol
   const homoeroticBullying = createTheme({
+    // Lumi's color scheme:
+    // 1. #D8E2DC
+    // 2. #FFE5D9
+    // 3. #FFCAD4
+    // 4. #F4ACB7
+    // 5. #9D8189
     palette: {
       mode: "light",
-      background: { default: "#000", paper: "#000", object: "#000" },
-      //   text: {},
-      //   primary: {},
+      background: { default: "#FFE5D9", paper: "#fff4f7", object: "#FFE5D9" },
+      text: { primary: "#5c474e" },
+      // constrast-adjusted from 1.
+      primary: { main: "#4f6a5a" },
       //   secondary: {},
     },
     typography: {
