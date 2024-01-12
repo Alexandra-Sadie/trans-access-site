d2af78281be2b6c06e8b56432af708d03cd2b949
s-avery <s_avery@riseup.net>
2024-01-12 17:54:59 -0500

    fixed bug using useEffect

--- src/components/Navbar/DarkModeSwitcher.js
+++ src/components/Navbar/DarkModeSwitcher.js
@@ -1,43 +1,54 @@
 // !IMPORT ZONE
 import { Button } from "@mui/material";
 import { DarkModeOutlined, LightModeOutlined } from "@mui/icons-material";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 // our context
 import { CustomThemesContext } from "../../providers/CustomThemesProvider";
 
 const DarkModeSwitcher = () => {
-  const { setCurrentTheme, homoeroticBullying, twinPeaksCharacter } =
-    useContext(CustomThemesContext);
+  const {
+    setCurrentTheme,
+    homoeroticBullying,
+    twinPeaksCharacter,
+    currentTheme,
+  } = useContext(CustomThemesContext);
 
-  // attempting to destructure/de-state-ify currentTheme to see if the switcher interacts with it better that way -- i think there might be a dom/react desync issue
-  const [themeMode, setThemeMode] = useState("");
+  // this useEffect and daisy-chain state exist to make the conditional rendering on the dark/light mode work -- just using state runs into sync issues with the DOM
+  // "currentTheme" is the state that gets fed to MUI's theming engine, while "themeMode" is a state that just exists on this page, for the sole purpose of being accessibly by useEffect and being used for conditional rendering of the button
+
+  const [themeMode, setThemeMode] = useState("light");
+
+  useEffect(() => {
+    if (currentTheme === homoeroticBullying) {
+      setThemeMode("light");
+    } else if (currentTheme === twinPeaksCharacter) {
+      setThemeMode("light");
+    }
+  }, [currentTheme]);
+
   return (
     <>
-      {/* {currentTheme === homoeroticBullying ? ( */}
-      <Button
-        sx={{ color: "primary.contrastText" }}
-        onClick={() => {
-          setCurrentTheme(twinPeaksCharacter);
-          setThemeMode("dark");
-          console.log(themeMode);
-        }}
-      >
-        <DarkModeOutlined />
-      </Button>
-      {/* ) : ( */}
-      <Button
-        sx={{ color: "primary.contrastText" }}
-        onClick={() => {
-          setCurrentTheme(homoeroticBullying);
-          setThemeMode("light");
-          setThemeMode("light");
-
-          console.log(themeMode);
-        }}
-      >
-        <LightModeOutlined />
-      </Button>
-      {/* )} */}
+      {themeMode === "light" ? (
+        <Button
+          sx={{ color: "primary.contrastText" }}
+          onClick={() => {
+            setCurrentTheme(twinPeaksCharacter);
+            setThemeMode("dark");
+          }}
+        >
+          <DarkModeOutlined />
+        </Button>
+      ) : (
+        <Button
+          sx={{ color: "primary.contrastText" }}
+          onClick={() => {
+            setCurrentTheme(homoeroticBullying);
+            setThemeMode("light");
+          }}
+        >
+          <LightModeOutlined />
+        </Button>
+      )}
     </>
   );
 };
