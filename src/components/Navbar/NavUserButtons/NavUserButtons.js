7229a5bd216b9bbee3b013232a7343f1ae0b7195
s-avery <s_avery@riseup.net>
2024-01-12 19:37:30 -0500

    button w acct icon

--- src/components/Navbar/NavUserButtons/NavUserButtons.js
+++ src/components/Navbar/NavUserButtons/NavUserButtons.js
@@ -3,6 +3,7 @@ import { AuthContext } from "../../../providers/AuthProvider";
 import { Button, Stack } from "@mui/material";
 import { Link as RouterLink, useNavigate } from "react-router-dom";
 import { routingObject } from "../../../routing";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 
 // ? i'd be open to a better name for this
 const NavUserButtons = () => {
@@ -10,6 +11,9 @@ const NavUserButtons = () => {
   const navigate = useNavigate();
   return (
     <Stack direction="row" spacing={2}>
+      <Button sx={{ color: "primary.contrastText" }}>
+        <PersonOutlineOutlinedIcon />
+      </Button>
       {user ? (
         <>
           {/* TODO - break these individual buttons into components in some elegant way */}
