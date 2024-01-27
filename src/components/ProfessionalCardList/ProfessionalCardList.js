a7ae73505815af5880f8c564f26fabee436e5f65
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2024-01-26 23:25:49 -0500

    ok now it works to load in the blank cards as a backup INSIDE the display logic

--- src/components/ProfessionalCardList/ProfessionalCardList.js
+++ src/components/ProfessionalCardList/ProfessionalCardList.js
@@ -1,7 +1,53 @@
 import { Stack, List } from "@mui/material";
 import ProfessionalCard from "./ProfessionalCard";
 
+const blankCardArr = [
+  {
+    uid: "0",
+    name: "",
+    profession: "",
+    location: "",
+    available: true,
+    waitTime: 0,
+    bio: "",
+    avatar: "",
+  },
+  {
+    uid: "1",
+    name: "",
+    profession: "",
+    location: "",
+    available: true,
+    waitTime: 0,
+    bio: "",
+    avatar: "",
+  },
+  {
+    uid: "2",
+    name: "",
+    profession: "",
+    location: "",
+    available: true,
+    waitTime: 0,
+    bio: "",
+    avatar: "",
+  },
+  {
+    uid: "3",
+    name: "",
+    profession: "",
+    location: "",
+    available: true,
+    waitTime: 0,
+    bio: "",
+    avatar: "",
+  },
+];
+
 const ProfessionalCardList = ({ cardArr }) => {
+  if (!cardArr) {
+    cardArr = blankCardArr;
+  }
   return (
     // Stack component="List"
     // gives us access to spacing, flex, etc properties of Stack
--- src/components/pages/Doctor.js
+++ src/components/pages/Doctor.js
@@ -1,61 +1,10 @@
 import ProfessionalCardList from "../ProfessionalCardList/ProfessionalCardList";
 import useCollection from "../../hooks/useCollection";
 
-const blankCardArr = [
-  {
-    uid: "0",
-    name: "",
-    profession: "",
-    location: "",
-    available: true,
-    waitTime: 0,
-    bio: "",
-    avatar: "",
-  },
-  {
-    uid: "1",
-    name: "",
-    profession: "",
-    location: "",
-    available: true,
-    waitTime: 0,
-    bio: "",
-    avatar: "",
-  },
-  {
-    uid: "2",
-    name: "",
-    profession: "",
-    location: "",
-    available: true,
-    waitTime: 0,
-    bio: "",
-    avatar: "",
-  },
-  {
-    uid: "3",
-    name: "",
-    profession: "",
-    location: "",
-    available: true,
-    waitTime: 0,
-    bio: "",
-    avatar: "",
-  },
-];
-
 const Doctor = ({ path }) => {
   const { currentCollection } = useCollection(path);
 
-  return (
-    <>
-      {currentCollection ? (
-        <ProfessionalCardList cardArr={blankCardArr} />
-      ) : (
-        <ProfessionalCardList cardArr={blankCardArr} />
-      )}
-    </>
-  );
+  return <ProfessionalCardList cardArr={currentCollection} />;
 };
 
 export default Doctor;
