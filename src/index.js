65b8c1d66e072a9888c1998184316a960bbcf7f7
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2024-02-01 17:25:55 -0500

    provider in the cascade, it builds, no calls yet

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ import NameChange from "./components/pages/NameChange";
 import Doctor from "./components/pages/Doctor";
 import LoginSignup from "./components/pages/LoginSignup/LoginSignup";
 import MyAccount from "./components/pages/MyAccount";
+import CollectionProvider from "./providers/CollectionProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -46,62 +47,66 @@ root.render(
     I have wrapped BrowserRouter in our LoggedInProvider so that it can give context on the user's logged in status to all routes in the app
  */}
     <AuthProvider>
-      {/* //TODO explain CardsProvider literally at all */}
-      {/* //TODO explain this one too lol */}
-      <CustomThemesProvider>
-        <BrowserRouter>
-          <Routes>
-            {/* 
+      {/* //TODO explain CollectionProvider literally at all */}
+      <CollectionProvider>
+        {/* //TODO explain this one too lol */}
+        <CustomThemesProvider>
+          <BrowserRouter>
+            <Routes>
+              {/* 
         We've structured our routes (AKA our pages) as nested sub-routes "inside" of App.js. This lets us use the React Router Outlet feature to load universal content in App.js which will be visible on every sub-route (AKA every page). We use this to make the Navbar load identically everywhere.
         See comments on app.js for further info. 
         docs: https://reactrouter.com/en/6.10.0/components/outlet
         */}
-            <Route element={<App />}>
-              <Route path={routingObject.main.route} element={<Main />} />
+              <Route element={<App />}>
+                <Route path={routingObject.main.route} element={<Main />} />
 
-              <Route
-                path={routingObject.intervention.route}
-                // Intervention ALSO receives a path prop because
-                // react router doesnt directly pass props of Route to the route element
-                // i.e., to have access to the path in the element, it needs the prop directly
-                element={
-                  <Intervention path={routingObject.intervention.route} />
-                }
-              />
+                <Route
+                  path={routingObject.intervention.route}
+                  // Intervention ALSO receives a path prop because
+                  // react router doesnt directly pass props of Route to the route element
+                  // i.e., to have access to the path in the element, it needs the prop directly
+                  element={
+                    <Intervention path={routingObject.intervention.route} />
+                  }
+                />
 
-              <Route
-                path={routingObject.therapist.route}
-                element={<Therapist path={routingObject.therapist.route} />}
-              />
+                <Route
+                  path={routingObject.therapist.route}
+                  element={<Therapist path={routingObject.therapist.route} />}
+                />
 
-              <Route
-                path={routingObject.name_change.route}
-                element={<NameChange path={routingObject.name_change.route} />}
-              />
+                <Route
+                  path={routingObject.name_change.route}
+                  element={
+                    <NameChange path={routingObject.name_change.route} />
+                  }
+                />
 
-              <Route
-                path={routingObject.doctor.route}
-                element={<Doctor path={routingObject.doctor.route} />}
-              />
+                <Route
+                  path={routingObject.doctor.route}
+                  element={<Doctor path={routingObject.doctor.route} />}
+                />
 
-              <Route
-                path={routingObject.login_signup.route}
-                element={<LoginSignup />}
-              />
+                <Route
+                  path={routingObject.login_signup.route}
+                  element={<LoginSignup />}
+                />
 
-              <Route
-                path={routingObject.my_account.route}
-                element={<MyAccount />}
-              />
-            </Route>
+                <Route
+                  path={routingObject.my_account.route}
+                  element={<MyAccount />}
+                />
+              </Route>
 
-            {/* 
+              {/* 
         // TODO: either make this properly fall through to full home page OR make custom error page 
         for now, people will see the toolbar with links to leave*/}
-            <Route path="*" element={<App />} />
-          </Routes>
-        </BrowserRouter>
-      </CustomThemesProvider>
+              <Route path="*" element={<App />} />
+            </Routes>
+          </BrowserRouter>
+        </CustomThemesProvider>
+      </CollectionProvider>
     </AuthProvider>
   </React.StrictMode>
 );
