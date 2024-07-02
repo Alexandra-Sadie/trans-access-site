9e63d56ba5ef9b34cd8fc3d9d556ab4c8a4057c5
s-avery <s_avery@riseup.net>
2024-07-01 20:29:42 -0400

    Just One Big Commit Babey

--- src/components/pages/NameChange.js
+++ src/components/pages/NameChange.js
@@ -1,5 +1,31 @@
+// import paper
+import Paper from "@mui/material/Paper";
+
 const NameChange = () => {
-  return <div>Name Change</div>;
+  // some functions yaaaay
+  //   make up a local elevation variable
+  let elevationVariable = Array.from(new Array(25));
+  console.log(elevationVariable);
+  return (
+    <>
+      <Paper elevation={3} sx={{ maxWidth: "400px", height: "100px" }}>
+        test
+      </Paper>
+      {elevationVariable.map((_thisdoesntmatter, elevationValue) => (
+        <Paper
+          elevation={elevationValue}
+          sx={{ maxWidth: "400px", height: "100px", margin: "24px" }}
+        >
+          {elevationValue}
+        </Paper>
+      ))}
+      {/*
+  
+  have it iterate on itself 24 times and each time increase its count by 1
+  and make a paper component with elevation (mui trait) equal to itself
+  */}
+    </>
+  );
 };
 
 export default NameChange;
