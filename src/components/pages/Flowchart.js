eeab9f06c332f4b68dbf2de26f5d09fb8cb27d08
s-avery <s_avery@riseup.net>
2024-07-14 13:13:29 -0400

    text for finch path exists

--- src/components/pages/Flowchart.js
+++ src/components/pages/Flowchart.js
@@ -1,5 +1,5 @@
 // !IMPORT ZONE
-import { Button, Stack, Typography } from "@mui/material";
+import { Button, Stack, Typography, Box } from "@mui/material";
 
 const Flowchart = () => {
   // so I'm looking at react mui drawers: https://mui.com/material-ui/react-drawer/ -- there are a couple options
@@ -11,19 +11,44 @@ const Flowchart = () => {
     <>
       <Stack sx={{ pt: 6 }} spacing={1.5} alignItems="center">
         <Typography variant="h2">Why is my cat yelling?</Typography>
-        <Typography>Which cat is yelling?</Typography>
-        <Stack
-          direction="row"
-          // this spacing property uses the spacing unit set in the MUI theme
-          // docs: https://mui.com/material-ui/customization/spacing/
-          spacing={2}
-          // this means the Stack will be display:none at mobile breakpoints and display:flex on desktop breakpoints
-        >
-          {/* These buttons should have some logic to make them be the same size but I'm doing that rn lol */}
-          <Button variant="contained">Cricket</Button>
-          {/* I am just giong to code the "Finch" path first */}
-          <Button variant="contained">Finch</Button>
-        </Stack>
+        {/* Putting this in a box (mui div) so that when I apply vanilla react state/conditional logic, this box can appear or disappear based on said logic*/}
+        {/* Maybe this could/should be a Stack or a more directly relevant MUI component? */}
+        <Box>
+          <Typography>Which cat is yelling?</Typography>
+          <Stack direction="row" spacing={2}>
+            {/* I am just giong to code the "Finch" path first */}
+            {/* These buttons should have some logic to make them be the same size but I'm doing that rn lol */}
+            <Button variant="contained">Cricket</Button>
+            <Button variant="contained">Finch</Button>
+          </Stack>
+        </Box>
+
+        <Box>
+          <Typography>Is she alone in a room?</Typography>
+          <Stack direction="row" spacing={2}>
+            <Button variant="contained">Yes</Button>
+            <Button variant="contained">No</Button>
+          </Stack>
+        </Box>
+
+        <Box>
+          <Typography>
+            She just woke up from a nap and is like where are you guuuuuys am I
+            alone...........
+          </Typography>
+          <Typography>
+            Make a sound so she know's you're in the other room and stpos
+            freaking out.
+          </Typography>
+        </Box>
+
+        <Box>
+          <Typography>
+            Either there is a bird in the window or Cricket and Finch are
+            playfighting.
+          </Typography>
+          <Typography>Let them have fun. :)</Typography>
+        </Box>
       </Stack>
     </>
   );
