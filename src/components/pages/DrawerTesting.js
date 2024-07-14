5a1f3ad09520af35c082d5ee72f5702dbee38dfa
s-avery <s_avery@riseup.net>
2024-07-14 17:22:48 -0400

    got trueThings to render in the drawer

--- src/components/pages/DrawerTesting.js
+++ src/components/pages/DrawerTesting.js
@@ -20,8 +20,9 @@ import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
 import { Stack } from "@mui/material";
 
-// my imports
-import Flowchart from "./Flowchart/Flowchart";
+//! my imports
+import { Button } from "@mui/material";
+import { useState } from "react";
 
 const drawerWidth = 240;
 const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
@@ -60,6 +61,26 @@ const DrawerHeader = styled("div")(({ theme }) => ({
 }));
 
 export default function PersistentDrawerRight() {
+  //! states
+  const [finchYelling, setFinchYelling] = useState(false);
+  // this one has default state of "" because we want it to appear on page load
+  const [displayWhoIsYellingQuestion, setDisplayWhoIsYellingQuestion] =
+    useState("");
+  // the rest have default state of "none" because they will display:none by default
+  // this could probably be done more functionally, without using these states, by having the buttons directly display:none the elements in question
+  const [displayIsFinchAloneQuestion, setDisplayIsFinchAloneQuestion] =
+    useState("none");
+  const [displayFinchIsAloneEndpoint, setDisplayFinchIsAloneEndpoint] =
+    useState("none");
+  const [
+    displayBirdOrPlayfightingEndpoint,
+    setDisplayBirdOrPlayfightingEndpoint,
+  ] = useState("none");
+
+  // states to put things that are true and things to be done in the drawer
+  const [trueThings, setTrueThings] = useState([]);
+  const [thingsToDo, setThingsToDo] = useState([]);
+
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
 
@@ -88,7 +109,80 @@ export default function PersistentDrawerRight() {
         </IconButton>
       </Toolbar>
       <Main open={open}>
-        <Flowchart />
+        <>
+          {/* //* FLOWCHART QUESTIONS */}
+          <Stack sx={{ pt: 6 }} spacing={1.5} alignItems="center">
+            <Typography variant="h2">Why is my cat yelling?</Typography>
+            {/* Putting this in a box (mui div) so that when I apply vanilla react state/conditional logic, this box can appear or disappear based on said logic*/}
+            {/* Maybe this could/should be a Stack or a more directly relevant MUI component? */}
+
+            <Box display={displayWhoIsYellingQuestion}>
+              <Typography>Which cat is yelling?</Typography>
+              <Stack direction="row" spacing={2}>
+                {/* I am just giong to code the "Finch" path first */}
+                {/* These buttons should have some logic to make them be the same size but I'm doing that rn lol */}
+                <Button variant="contained">Cricket (Coming Soon™️)</Button>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    // I'm leaving this state here, because state tracking + ternary rendering was my first thought on how to go through the questions, but i'm going to implement another idea that i think might work better, detailed below
+                    setFinchYelling(true);
+                    // hides itself
+                    setDisplayWhoIsYellingQuestion("none");
+                    setDisplayIsFinchAloneQuestion("");
+                    // adds "finch is yelling" to "things that are true"
+                    setTrueThings([...trueThings, "Finch is yelling"]);
+                  }}
+                >
+                  Finch
+                </Button>
+              </Stack>
+            </Box>
+
+            <Box display={displayIsFinchAloneQuestion}>
+              <Typography>Is she alone in a room?</Typography>
+              <Stack direction="row" spacing={2}>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    setDisplayIsFinchAloneQuestion("none");
+                    setDisplayFinchIsAloneEndpoint("");
+                  }}
+                >
+                  Yes
+                </Button>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    setDisplayIsFinchAloneQuestion("none");
+                    setDisplayBirdOrPlayfightingEndpoint("");
+                  }}
+                >
+                  No
+                </Button>
+              </Stack>
+            </Box>
+
+            <Box display={displayFinchIsAloneEndpoint}>
+              <Typography>
+                She just woke up from a nap and is like where are you guuuuuys
+                am I alone...........
+              </Typography>
+              <Typography>
+                Make a sound so she know's you're in the other room and stpos
+                freaking out.
+              </Typography>
+            </Box>
+
+            <Box display={displayBirdOrPlayfightingEndpoint}>
+              <Typography>
+                Either there is a bird in the window or Cricket and Finch are
+                playfighting.
+              </Typography>
+              <Typography>Let them have fun. :)</Typography>
+            </Box>
+          </Stack>
+        </>{" "}
       </Main>
       <Drawer
         sx={{
@@ -114,7 +208,9 @@ export default function PersistentDrawerRight() {
         <Divider />
         <Typography fontSize="1.4rem">Things that are true</Typography>
         <List>
-          <Typography></Typography>
+          {trueThings.map((trueThing) => (
+            <ListItem key={trueThing}>{trueThing}</ListItem>
+          ))}
         </List>
         <Divider />
         <Typography fontSize="1.4rem">Things to do</Typography>
