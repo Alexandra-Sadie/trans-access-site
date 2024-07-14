392ed2eca4fd8dd720570a567cfe39de12e67e58
s-avery <s_avery@riseup.net>
2024-07-14 13:07:21 -0400

    started coding finch path

--- src/components/pages/Flowchart.js
+++ src/components/pages/Flowchart.js
@@ -1,5 +1,5 @@
 // !IMPORT ZONE
-import { Button } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 
 const Flowchart = () => {
   // so I'm looking at react mui drawers: https://mui.com/material-ui/react-drawer/ -- there are a couple options
@@ -9,7 +9,22 @@ const Flowchart = () => {
 
   return (
     <>
-      <Button>test</Button>
+      <Stack sx={{ pt: 6 }} spacing={1.5} alignItems="center">
+        <Typography variant="h2">Why is my cat yelling?</Typography>
+        <Typography>Which cat is yelling?</Typography>
+        <Stack
+          direction="row"
+          // this spacing property uses the spacing unit set in the MUI theme
+          // docs: https://mui.com/material-ui/customization/spacing/
+          spacing={2}
+          // this means the Stack will be display:none at mobile breakpoints and display:flex on desktop breakpoints
+        >
+          {/* These buttons should have some logic to make them be the same size but I'm doing that rn lol */}
+          <Button variant="contained">Cricket</Button>
+          {/* I am just giong to code the "Finch" path first */}
+          <Button variant="contained">Finch</Button>
+        </Stack>
+      </Stack>
     </>
   );
 };
