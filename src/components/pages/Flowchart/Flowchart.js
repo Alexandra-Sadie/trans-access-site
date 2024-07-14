0305b1b188ea569a37d15e90419a789b8a80a411
s-avery <s_avery@riseup.net>
2024-07-14 16:38:55 -0400

    finch path complete

--- src/components/pages/Flowchart/Flowchart.js
+++ src/components/pages/Flowchart/Flowchart.js
@@ -1,18 +1,27 @@
 // !IMPORT ZONE
 import { Button, Stack, Typography, Box } from "@mui/material";
 import { useState } from "react";
-import WhoIsYelling from "./Questions/WhoIsYelling";
-import IsFinchAlone from "./Questions/IsFinchAlone";
 
 const Flowchart = () => {
   // so I'm looking at react mui drawers: https://mui.com/material-ui/react-drawer/ -- there are a couple options
   // the "responsive drawer" seems closest to what we might want?
   // this being for having a checklist on the side that can keep trakc of what papers/etc somene needs as they fill out the quiz
   // i will implement basic flowchart functionality first and then examine this further, presumably we'll do this together
+
   // *STATE ZONE
   const [finchYelling, setFinchYelling] = useState(false);
-  const [displayWhoIsYelling, setDisplayWhoIsYelling] = useState("");
-  const [displayIsFinchAlone, setDisplayIsFinchAlone] = useState("none");
+  // this one has default state of "" because we want it to appear on page load
+  const [displayWhoIsYellingQuestion, setDisplayWhoIsYellingQuestion] =
+    useState("");
+  // the rest have default state of "none" because they will display:none by default
+  const [displayIsFinchAloneQuestion, setDisplayIsFinchAloneQuestion] =
+    useState("none");
+  const [displayFinchIsAloneEndpoint, setDisplayFinchIsAloneEndpoint] =
+    useState("none");
+  const [
+    displayBirdOrPlayfightingEndpoint,
+    setDisplayBirdOrPlayfightingEndpoint,
+  ] = useState("none");
 
   return (
     <>
@@ -20,19 +29,21 @@ const Flowchart = () => {
         <Typography variant="h2">Why is my cat yelling?</Typography>
         {/* Putting this in a box (mui div) so that when I apply vanilla react state/conditional logic, this box can appear or disappear based on said logic*/}
         {/* Maybe this could/should be a Stack or a more directly relevant MUI component? */}
-        <Box display={displayWhoIsYelling}>
+
+        <Box display={displayWhoIsYellingQuestion}>
           <Typography>Which cat is yelling?</Typography>
           <Stack direction="row" spacing={2}>
             {/* I am just giong to code the "Finch" path first */}
             {/* These buttons should have some logic to make them be the same size but I'm doing that rn lol */}
-            <Button variant="contained">Cricket</Button>
+            <Button variant="contained">Cricket (Coming Soon™️)</Button>
             <Button
               variant="contained"
               onClick={() => {
                 // I'm leaving this state here, because state tracking + ternary rendering was my first thought on how to go through the questions, but i'm going to implement another idea that i think might work better, detailed below
                 setFinchYelling(true);
                 // hides itself
-                setDisplayWhoIsYelling("none");
+                setDisplayWhoIsYellingQuestion("none");
+                setDisplayIsFinchAloneQuestion("");
               }}
             >
               Finch
@@ -40,15 +51,31 @@ const Flowchart = () => {
           </Stack>
         </Box>
 
-        <Box display={displayIsFinchAlone}>
+        <Box display={displayIsFinchAloneQuestion}>
           <Typography>Is she alone in a room?</Typography>
           <Stack direction="row" spacing={2}>
-            <Button variant="contained">Yes</Button>
-            <Button variant="contained">No</Button>
+            <Button
+              variant="contained"
+              onClick={() => {
+                setDisplayIsFinchAloneQuestion("none");
+                setDisplayFinchIsAloneEndpoint("");
+              }}
+            >
+              Yes
+            </Button>
+            <Button
+              variant="contained"
+              onClick={() => {
+                setDisplayIsFinchAloneQuestion("none");
+                setDisplayBirdOrPlayfightingEndpoint("");
+              }}
+            >
+              No
+            </Button>
           </Stack>
         </Box>
 
-        <Box>
+        <Box display={displayFinchIsAloneEndpoint}>
           <Typography>
             She just woke up from a nap and is like where are you guuuuuys am I
             alone...........
@@ -59,7 +86,7 @@ const Flowchart = () => {
           </Typography>
         </Box>
 
-        <Box>
+        <Box display={displayBirdOrPlayfightingEndpoint}>
           <Typography>
             Either there is a bird in the window or Cricket and Finch are
             playfighting.
