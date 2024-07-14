09a7b1304c6a2742e3abf2f4dacadfcf1ec44bfb
s-avery <s_avery@riseup.net>
2024-07-14 12:57:01 -0400

    comments on MUI drawer, starting with some buttons

--- src/components/pages/Flowchart.js
+++ src/components/pages/Flowchart.js
@@ -1,4 +1,16 @@
+// !IMPORT ZONE
+import { Button } from "@mui/material";
+
 const Flowchart = () => {
-  return <div>Flowchart</div>;
+  // so I'm looking at react mui drawers: https://mui.com/material-ui/react-drawer/ -- there are a couple options
+  // the "responsive drawer" seems closest to what we might want?
+  // this being for having a checklist on the side that can keep trakc of what papers/etc somene needs as they fill out the quiz
+  // i will implement basic flowchart functionality first and then examine this further, presumably we'll do this together
+
+  return (
+    <>
+      <Button>test</Button>
+    </>
+  );
 };
 export default Flowchart;
