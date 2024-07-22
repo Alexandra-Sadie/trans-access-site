c5bd2af3b62b28079dac86fbcee2062ad8458e30
s-avery <s_avery@riseup.net>
2024-07-22 12:39:17 -0400

    explaining name change tool

--- src/components/pages/NameChange/SplashScreen.js
+++ src/components/pages/NameChange/SplashScreen.js
@@ -1,16 +1,27 @@
-import { Typography, Button, Stack } from "@mui/material";
+import { Typography, Button, Stack, Link } from "@mui/material";
 
 const SplashScreen = ({ disableSplashScreen }) => {
   return (
     <Stack spacing={{ xs: 1, md: 2 }} alignItems="center" paddingTop={6}>
       <Typography variant="h2">Name Change Tool</Typography>
       <Typography variant="body1" maxWidth={"40vw"}>
-        Explanation of what this tool is: Lorem ipsum dolor sit, amet
-        consectetur adipisicing elit. Aperiam necessitatibus autem officia
-        asperiores libero, maxime sed. Sint, consequuntur at suscipit magni
-        ipsum laudantium blanditiis neque, veritatis optio laborum delectus.
-        Nostrum.
+        This is a tool to help trans people in Quebec better understand what
+        they need to get a legal name and/or gender marker change. The process
+        can be a bit complicated, and different documents or steps can be
+        required based on factors like age, citizenship status, or time spent in
+        Quebec.
       </Typography>
+      <Typography variant="body1" maxWidth={"40vw"}>
+        Because there are so many variables, we've made a short questionnaire to
+        determine what documents or steps you'll need. As you answer questions
+        about your situation, you'll see a checklist of requirements, which you
+        can save after the quiz to track your progress.
+      </Typography>
+      <Typography variant="body1" maxWidth={"40vw"}>
+        If you want, you can see the flowchart this is modeled after{" "}
+        <Link href="https://i.imgur.com/RlD4UQ8.png">here</Link>.
+      </Typography>
+
       <Button variant="contained" onClick={disableSplashScreen}>
         button
       </Button>
