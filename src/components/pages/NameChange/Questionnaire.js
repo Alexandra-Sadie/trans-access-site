11e3c22d6872d46103254f5d96db6f181c81b69a
s-avery <s_avery@riseup.net>
2024-07-22 19:19:41 -0400

    grid + paper implemented, pausing to implement type system

--- src/components/pages/NameChange/Questionnaire.js
+++ src/components/pages/NameChange/Questionnaire.js
@@ -4,10 +4,10 @@ const Questionnaire = () => {
   return (
     <>
       {/* note: this needs to change, md values should be 8 and 4, xs different - we need this for now as mobile view not built yet. */}
-      <Grid item xs={8}>
+      <Grid item xs={8} md={8}>
         <Card sx={{ minHeight: 200 }}>main question</Card>
       </Grid>
-      <Grid item xs={4}>
+      <Grid item xs={4} md={4}>
         <Card sx={{ minHeight: 96 }}>checklist</Card>
       </Grid>
     </>
--- src/components/pages/NameChange/SplashScreen.js
+++ src/components/pages/NameChange/SplashScreen.js
@@ -6,44 +6,46 @@ import {
   Link,
   CardHeader,
   CardContent,
+  Grid,
+  Paper,
 } from "@mui/material";
 
 const SplashScreen = ({ disableSplashScreen }) => {
   return (
-    <Stack alignItems="center" paddingTop={6}>
-      <Card
-        spacing={{ xs: 1, md: 2 }}
-        maxWidth="200px"
-        align="center"
-        alignItems="center"
-        elevation={4}
-      >
-        <CardContent>
-          <CardHeader title="Name Change"></CardHeader>
-          <Typography variant="body1" maxWidth={"40vw"}>
-            This is a tool to help trans people in Quebec better understand what
-            they need to get a legal name and/or gender marker change. The
-            process can be a bit complicated, and different documents or steps
-            can be required based on factors like age, citizenship status, or
-            time spent in Quebec.
-          </Typography>
-          <Typography variant="body1" maxWidth={"40vw"}>
-            Because there are so many variables, we've made a short
-            questionnaire to determine what documents or steps you'll need. As
-            you answer questions about your situation, you'll see a checklist of
-            requirements, which you can save after the quiz to track your
-            progress.
-          </Typography>
-          <Button variant="contained" onClick={disableSplashScreen}>
-            Begin
-          </Button>
-        </CardContent>
-      </Card>
-      <Typography variant="body1" align="center" maxWidth={"40vw"}>
-        If you want, you can see the flowchart this is modeled after here[LINK
-        TKTK].
-      </Typography>
-    </Stack>
+    <Grid item xs={12} md={12}>
+      <Paper align="center" alignItems="center" elevation={4}>
+        <Grid item md={8}>
+          <Stack spacing={{ xs: 1, md: 8 }}>
+            <Typography variant="h1">Name Change</Typography>
+            <Typography variant="h2">
+              Do you want to legally change your sex designation and/or name but
+              don’t know where to start?
+            </Typography>
+            <Typography variant="body1">
+              This is a tool to help trans people in Quebec better understand
+              what they need to get a legal name and/or gender marker change.
+              The process can be a bit complicated, and different documents or
+              steps can be required based on factors like age, citizenship
+              status, or time spent in Quebec.
+            </Typography>
+            <Typography variant="body1">
+              Because there are so many variables, we've made a short
+              questionnaire to determine what documents or steps you'll need. As
+              you answer questions about your situation, you'll see a checklist
+              of requirements, which you can save after the quiz to track your
+              progress.
+            </Typography>
+            <Button variant="contained" onClick={disableSplashScreen}>
+              Begin
+            </Button>
+            <Typography variant="body1" align="center">
+              If you want, you can see the flowchart this is modeled after
+              here[LINK TKTK].
+            </Typography>
+          </Stack>
+        </Grid>
+      </Paper>
+    </Grid>
   );
 };
 export default SplashScreen;
