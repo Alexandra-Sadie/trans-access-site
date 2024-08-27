2f8310ae65d128c73075ed2b743a6faf18d6bbae
ab-lopez <bella.abenoja95@gmail.com>
2024-08-26 20:04:52 -0400

    moved dark palette over

--- src/providers/CustomThemesProvider/CustomThemesProvider.js
+++ src/providers/CustomThemesProvider/CustomThemesProvider.js
@@ -22,7 +22,7 @@ const CustomThemesProvider = ({ children }) => {
     // 3. #FFCAD4
     // 4. #F4ACB7
     // 5. #9D8189
-    palette: palette,
+    palette: palette.light,
     typography: typescale,
   });
 
@@ -33,14 +33,7 @@ const CustomThemesProvider = ({ children }) => {
     // 3. #B4869F
     // 4. #985F6F
     // 5. #4E4C67
-    palette: {
-      mode: "dark",
-      background: { default: "#45435a", paper: "#4E4C67" },
-      text: { primary: "#eeebfc" },
-      //   the primary.main here is adjusted from #B4869F above -- a dif versin of it has primary.main as #A6B1E1 -- gives the site a more blue/cold tone, rather than pink/warm. i like this, but both Work
-      primary: { main: "#b689a2", contrastText: "#272633" },
-      secondary: { main: "#985F6F" },
-    },
+    palette: palette.dark,
     typography: typescale,
   });
 
--- src/providers/CustomThemesProvider/palette.js
+++ src/providers/CustomThemesProvider/palette.js
@@ -1,9 +1,19 @@
 const palette = {
+    light: {
     mode: "light",
     background: { default: "#FFE5D9", paper: "#fff9fa", object: "#FFE5D9" },
     text: { primary: "#5c474e" },
     primary: { main: "#9D8189", contrastText: "#271f22" },
     secondary: { main: "#F4ACB7" },
-  }
+  },
+    dark: {
+        mode: "dark",
+        background: { default: "#45435a", paper: "#4E4C67" },
+        text: { primary: "#eeebfc" },
+        //   the primary.main here is adjusted from #B4869F above -- a dif versin of it has primary.main as #A6B1E1 -- gives the site a more blue/cold tone, rather than pink/warm. i like this, but both Work
+        primary: { main: "#b689a2", contrastText: "#272633" },
+        secondary: { main: "#985F6F" },
+    }
+}
 
   export default palette;
