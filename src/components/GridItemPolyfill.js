936c57faf6962307d223016189343bdffe5fd7f4
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2024-08-29 23:08:52 -0400

    wrote test polyfill for proper grid2 syntax

---
+++ src/components/GridItemPolyfill.js
@@ -0,0 +1,10 @@
+import Grid from "@mui/material/Unstable_Grid2";
+
+const GridItemPolyfill = ({ size, children, ...props }) => {
+  return (
+    <Grid xs={size.xs} md={size.md} props>
+      {children}
+    </Grid>
+  );
+};
+export default GridItemPolyfill;
