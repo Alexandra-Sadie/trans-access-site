b8edcf9dd0243d98aa6d1aeaed43b9f3cae39a01
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2024-08-29 23:16:02 -0400

    i think THIS is how to spread props

--- src/components/GridItemPolyfill.js
+++ src/components/GridItemPolyfill.js
@@ -2,7 +2,7 @@ import Grid from "@mui/material/Unstable_Grid2";
 
 const GridItemPolyfill = ({ size, children, ...props }) => {
   return (
-    <Grid xs={size.xs} md={size.md} props>
+    <Grid xs={size.xs} md={size.md} {...props}>
       {children}
     </Grid>
   );
