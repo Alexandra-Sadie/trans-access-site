191e71e160d193c223bee97a924ee854b26b77cb
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2025-07-29 19:54:51 -0400

    test array

--- src/components/pages/NameChange.js
+++ src/components/pages/NameChange.js
@@ -1,20 +1,24 @@
-// import { Grid2 as Grid, Paper } from "@mui/material";
-import { Typography } from "@mui/material";
+import { Grid2 as Grid, Paper } from "@mui/material";
+// import { Typography } from "@mui/material";
 
 const NameChange = () => {
   return (
     <>
-      <Typography variant="h2">Bientôt disponible !</Typography>
+      {/* <Typography variant="h2">Bientôt disponible !</Typography>
       <Typography>
         Notre outil pour vous aider à trouver les étapes et les documents dont
         vous aurez besoin pour obtenir un changement de nom ou de marque de
         genre sera lancé à l'été 2025.
-      </Typography>
-      {/* old grid test array: {[...new Array(12)].map((_, i) => (
-        <Grid size={{ xs: 1, md: 1 }}>
-          <Paper>{i}</Paper>
+      </Typography> */}
+      {[...new Array(12)].map((_, i) => (
+        <Grid size={{ md: 12 }}>
+          <Paper sx={{ height: "150px" }}>
+            <Grid offset={{ md: i }}>
+              <Paper>{i + 1}</Paper>
+            </Grid>
+          </Paper>
         </Grid>
-      ))} */}
+      ))}
     </>
   );
 };
