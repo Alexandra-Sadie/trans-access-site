188beaf194bd6ce48f5c4bc464390b7ec5492bc7
Aurora Becerra Granados <78286941+eclectic-kitty@users.noreply.github.com>
2025-07-29 20:54:29 -0400

    reached the bottom of our pit of explorations 🕳️

--- src/components/pages/NameChange.js
+++ src/components/pages/NameChange.js
@@ -4,21 +4,26 @@ import { Grid2 as Grid, Paper } from "@mui/material";
 const NameChange = () => {
   return (
     <>
+      <Grid container size={8} columns={8}>
+        <Grid size={8}>
+          <Paper sx={{ height: 100 }}>quiz! much wow!</Paper>
+        </Grid>
+      </Grid>
       {/* <Typography variant="h2">Bientôt disponible !</Typography>
       <Typography>
         Notre outil pour vous aider à trouver les étapes et les documents dont
         vous aurez besoin pour obtenir un changement de nom ou de marque de
         genre sera lancé à l'été 2025.
       </Typography> */}
-      {[...new Array(12)].map((_, i) => (
-        <Grid size={{ md: 12 }}>
+      {/* {[...new Array(12)].map((_, i) => (
+        <Grid size={{ md: 6 }} offset={{ md: 6 }}>
           <Paper sx={{ height: "150px" }}>
             <Grid offset={{ md: i }}>
               <Paper>{i + 1}</Paper>
             </Grid>
           </Paper>
         </Grid>
-      ))}
+      ))} */}
     </>
   );
 };
