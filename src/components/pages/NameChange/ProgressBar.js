d869a06c5b41664b6467c67fa7215567fb2dfaa2
Aurora Becerra Granados <78286941+eclectic-kitty@users.noreply.github.com>
2025-08-04 17:02:37 -0400

    Added Checklist Button

--- src/components/pages/NameChange/ProgressBar.js
+++ src/components/pages/NameChange/ProgressBar.js
@@ -1,6 +1,5 @@
-import { Paper } from "@mui/material";
-// Not sure if we should import the theme in here...
-// As it stands, it seems to be getting it from above, so I think we're ok, but it does mean this wouldn't work without *our* theme and *our* colour names? Is that bad?
+import { Stack, Paper, IconButton } from "@mui/material";
+import { FormatListBulleted } from "@mui/icons-material";
 
 /* This is a custom progress bar that can 
 
@@ -24,27 +23,37 @@ const ProgressBar = ({ value, custHeight, custPadding }) => {
   return (
     <Paper // This is the Paper to hold the progress bar and checklist button
       elevation={4}
-      sx={{ height: 72, paddingY: 3, paddingX: 6 }}
+      sx={{ height: 72, paddingY: 3, paddingLeft: 6, paddingRight: 3 }}
     >
-      <Paper // This is the bar's background
-        elevation={4}
-        sx={{
-          backgroundColor: "progressBar.light",
-          // Not sure if we actually want the size and padding to be editable...
-          // Also aware we probs need different values for mobile and desktop
-          height: "100%",
-          padding: custPadding,
-        }}
+      <Stack
+        direction="row"
+        spacing={3}
+        sx={{ alignItems: "center", height: "100%" }}
       >
-        <Paper // This is the inner bar that represents the amount completed
+        <Paper // This is the bar's background
           elevation={4}
           sx={{
-            backgroundColor: "progressBar.main",
-            height: "100%",
-            width: value,
+            backgroundColor: "progressBar.light",
+            // Not sure if we actually want the size and padding to be editable...
+            // Also aware we probs need different values for mobile and desktop
+            height: "100%", // Not sure why I need to be explicit about height with both the Stack and the Paper...
+            padding: custPadding,
+            width: "100%",
           }}
-        ></Paper>
-      </Paper>
+        >
+          <Paper // This is the inner bar that represents the amount completed
+            elevation={4}
+            sx={{
+              backgroundColor: "progressBar.main",
+              height: "100%",
+              width: "50%",
+            }}
+          ></Paper>
+        </Paper>
+        <IconButton aria-label="open checklist" size="small">
+          <FormatListBulleted />
+        </IconButton>
+      </Stack>
     </Paper>
   );
 };
