d4a39fa1c3c70fbb48787b8565a4dcdfd3daa3f2
Aurora Becerra Granados <78286941+eclectic-kitty@users.noreply.github.com>
2025-08-04 15:07:21 -0400

    Laid out Quiz

--- src/components/pages/NameChange/Quiz.js
+++ src/components/pages/NameChange/Quiz.js
@@ -1,7 +1,59 @@
-import { Grid2 as Grid, Stack, Paper, Card, Typography } from "@mui/material";
+import {
+  Grid2 as Grid,
+  Stack,
+  Paper,
+  Card,
+  Typography,
+  Button,
+} from "@mui/material";
+
+import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
 
 const Quiz = () => {
-  return <Paper elevation={4}>Quiz will go here!</Paper>;
+  return (
+    <Paper elevation={4} sx={{ paddingY: 10, paddingX: 6 }}>
+      <Stack
+        spacing={7}
+        sx={{ justifyContent: "center", alignItems: "stretch" }}
+      >
+        <Typography variant="h2" component="h1" align="center">
+          {/* Not sure which header scale to use here */}
+          Have you acquired and submitted all the documents in your checklist?
+        </Typography>
+        <Typography variant="body1" align="left">
+          Once you’ve submitted everything, the État Civil will render a
+          decision.
+        </Typography>
+        <Button
+          variant="contained"
+          size="large"
+          sx={{ bgcolor: "secondary.main" }}
+        >
+          They approved
+        </Button>
+        <Button
+          variant="contained"
+          size="large"
+          sx={{ bgcolor: "secondary.main" }}
+        >
+          They refused
+        </Button>
+        <Stack
+          direction="row"
+          spacing={1}
+          sx={{ alignItems: "center", justifyContent: "center" }}
+        >
+          <Button variant="contained" size="small">
+            <ArrowBackIos fontSize="small" />{" "}
+            {/* no idea why it's displaying off-center */}
+          </Button>
+          <Button variant="contained" size="small">
+            <ArrowForwardIos fontSize="small" />
+          </Button>
+        </Stack>
+      </Stack>
+    </Paper>
+  );
 };
 
 export default Quiz;
