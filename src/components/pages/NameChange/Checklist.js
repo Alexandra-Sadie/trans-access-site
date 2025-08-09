65dc6f31f7ead4eca6764953bc79c2425a11e638
Aurora Becerra Granados <78286941+eclectic-kitty@users.noreply.github.com>
2025-08-09 14:29:22 -0400

    Checklist header

--- src/components/pages/NameChange/Checklist.js
+++ src/components/pages/NameChange/Checklist.js
@@ -1,7 +1,21 @@
-import { Grid2 as Grid, Stack, Paper, Card, Typography } from "@mui/material";
+import { Stack, Paper, Card, Typography } from "@mui/material";
+import { Close } from "@mui/icons-material";
 
 const Checklist = () => {
-  return <Paper elevation={4}>Checklist will go here!</Paper>;
+  return (
+    <Paper elevation={4} sx={{ paddingTop: 6, paddingX: 2, height: 688 }}>
+      <Stack
+        direction="row"
+        sx={{ justifyContent: "space-between", alignItems: "center" }}
+      >
+        <Typography variant="h6" component="h2">
+          Your checklist
+        </Typography>
+        <Close />
+      </Stack>
+      <Stack spacing={4}></Stack>
+    </Paper>
+  );
 };
 
 export default Checklist;
