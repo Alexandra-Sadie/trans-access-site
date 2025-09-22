fd96bdea6bb6012f937047c1e3a10d87bb591d8a
Aurora Becerra Granados <78286941+eclectic-kitty@users.noreply.github.com>
2025-09-22 14:48:40 -0400

    Removed seemingly unnecessary box

--- src/components/pages/NameChange/Checklist.js
+++ src/components/pages/NameChange/Checklist.js
@@ -27,43 +27,45 @@ const Checklist = ({
             height: "100%",
             zIndex: "99",
             right: 0,
+            // TODO padding: 3 is an eyeball based on the design having ~24px of padding -- this may need to change for consistency with ProgressBar, ideally the buttons are at the same place on the screen
+            padding: 3,
           }}
         >
-          <Box
-            // TODO this Box stops the Stacks below from inherting "height: 100%" -- idk ifthats the best way to do it
+          {/* <Box
+            //  this Box stops the Stacks below from inherting "height: 100%" -- idk ifthats the best way to do it
             // also width=100% makes it fill the paper
-            // TODO padding: 3 is an eyeball based on the design having ~24px of padding -- this may need to change for consistency with ProgressBar, ideally the buttons are at the same place on the screen
             width="100%"
+            // Au-Update: The height seems to be working fine without this?
             sx={{ padding: 3 }}
-          >
-            <Stack direction="row" justifyContent="space-between">
-              <Typography
-                // TODO confirm this in the figma -- i'm not sure i have access to it due to our new account setup
-                component="h2"
-                variant="h6"
-              >
-                Your Checklist
-              </Typography>
-              <IconButton
-                aria-label="close checklist"
-                onClick={() => {
-                  setDisplayModalBoolean(false);
-                }}
-              >
-                <Close />
-              </IconButton>
-            </Stack>
+          > */}
+          <Stack direction="row" justifyContent="space-between">
+            <Typography
+              // TODO confirm this in the figma -- i'm not sure i have access to it due to our new account setup
+              component="h2"
+              variant="h6"
+            >
+              Your Checklist
+            </Typography>
+            <IconButton
+              aria-label="close checklist"
+              onClick={() => {
+                setDisplayModalBoolean(false);
+              }}
+            >
+              <Close />
+            </IconButton>
+          </Stack>
 
-            <Stack>
-              {currentChecklistItems
-                //TODO this flattens one level of the nested array-- double check that is correct and we won't run into any issues
-                // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
-                .flat() // currentChecklistItems is a 2d array for data logic reasons, so it must first be flattened in order to be displayed
-                .map((item) => (
-                  <ChecklistCard key={item.checkItemHeader} {...item} />
-                ))}
-            </Stack>
-          </Box>
+          <Stack>
+            {currentChecklistItems
+              //TODO this flattens one level of the nested array-- double check that is correct and we won't run into any issues
+              // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
+              .flat() // currentChecklistItems is a 2d array for data logic reasons, so it must first be flattened in order to be displayed
+              .map((item) => (
+                <ChecklistCard key={item.checkItemHeader} {...item} />
+              ))}
+          </Stack>
+          {/* </Box> */}
         </Paper>
       </ClickAwayListener>
     ) : (
