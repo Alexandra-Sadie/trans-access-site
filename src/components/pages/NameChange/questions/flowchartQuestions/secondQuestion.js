545f897e48cedaf2ccce075b6937560b0e2076f9
Aurora Becerra Granados <78286941+eclectic-kitty@users.noreply.github.com>
2025-11-10 17:59:05 -0500

    Added second question

---
+++ src/components/pages/NameChange/questions/flowchartQuestions/secondQuestion.js
@@ -0,0 +1,30 @@
+import { thirdQuestion } from "./thirdQuestion";
+
+export const secondQuestion = {
+  questionHeader: "Have you been domiciled in Quebec for at least one year?", // I feel like we should use "living" here instead...
+  questionSubHeader: "",
+  answers: [
+    {
+      buttonText: "Yes",
+      checklistItems: [
+        {
+          checkItemHeader: "Two proofs of address",
+          checkItemBodyText: "", // Not sure what needs to go here
+          checkItemButton: {
+            // Same here
+            text: "",
+            link: "",
+          },
+        },
+      ],
+      nextQuestion: thirdQuestion,
+      done: false,
+    },
+    {
+      buttonText: "No",
+      checklistItems: [],
+      //nextQuestion: , //Another ending!
+      done: true,
+    },
+  ],
+};
