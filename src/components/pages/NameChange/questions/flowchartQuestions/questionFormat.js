3f5bdcc7d2ea6ec02b6c42e21dc032755c764f63
Aurora Becerra Granados <78286941+eclectic-kitty@users.noreply.github.com>
2025-11-10 18:00:56 -0500

    file for data structure templates

---
+++ src/components/pages/NameChange/questions/flowchartQuestions/questionFormat.js
@@ -0,0 +1,26 @@
+import { Question } from "./Question";
+
+export const Question = 
+{
+  qHeading: "",
+  qSubHeading: "",
+  answers: [
+    {
+      buttonText: "",
+      checklistItems: [],
+      nextQuestion: ,
+      done: ,
+    },
+  ],
+}
+
+type ChecklistItem = 
+{
+  checkItemHeader: "",
+  checkItemBodyText: "", 
+  checkItemButton: {
+    text: "",
+    link: "",
+  }, 
+}
+x
