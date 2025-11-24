769196b393c522760654fb226cd3be57728ee041
Aurora Becerra Granados <78286941+eclectic-kitty@users.noreply.github.com>
2025-11-24 04:06:47 -0500

    Began filling out question

---
+++ src/components/pages/NameChange/questions/flowchartQuestions/childrenOfAge.js
@@ -0,0 +1,18 @@
+export const childrenOfAge = {
+  questionHeader: "Do you have children?",
+  questionSubHeader: "",
+  answers: [
+    {
+      buttonText: "Yes",
+      checklistItems: [],
+      nextQuestion: {},
+      done: false,
+    },
+    {
+      buttonText: "No",
+      checklistItems: [],
+      nextQuestion: {},
+      done: false,
+    },
+  ],
+};
