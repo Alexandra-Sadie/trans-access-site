28fab1d055c3a68b3494572cd2e56bdfba35b6ae
s-avery <s_avery@riseup.net>
2025-11-25 14:51:40 -0500

    potential data structure for 18+ variant

--- src/components/pages/NameChange/questions/secondQuestion.js
+++ src/components/pages/NameChange/questions/secondQuestion.js
@@ -1,13 +1,24 @@
+import { firstQuestion } from "./firstQuestion";
+import { useContext } from "react";
+import { NameChangeContext } from "../../../../providers/NameChangeProvider";
+const { ofAgeBoolean } = useContext(NameChangeContext);
+
 export const secondQuestion = {
   questionHeader:
     "This should lead to a different next question if you're under 18 or 18+.",
   questionSubHeader: "Lorem ipsum.",
   answers: [
     {
-      buttonText: "Next Question!",
+      buttonText: "Did it work?",
       checklistItems: [],
+      ofAgeChecklistItems: [],
       nextQuestion: {
-        questionHeader: "This is the end.",
+        questionHeader: "Under 18.",
+        questionSubHeader: "This is the end of the current path.",
+        answers: [],
+      },
+      ofAgeNextQuestion: {
+        questionHeader: "Over 18.",
         questionSubHeader: "This is the end of the current path.",
         answers: [],
       },
