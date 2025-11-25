4d4c560f0d9ecc84d343831f9444354fae4759fe
s-avery <s_avery@riseup.net>
2025-11-25 13:43:49 -0500

    changed 1st question to how old r u

--- src/components/pages/NameChange/questions/firstQuestion.js
+++ src/components/pages/NameChange/questions/firstQuestion.js
@@ -1,11 +1,11 @@
 import { secondQuestion } from "./secondQuestion";
 export const firstQuestion = {
-  questionHeader: "This is the first mandatory question.",
+  questionHeader: "How old are you?",
   questionSubHeader:
     "The 'add checklist item' button will take you to a nested subquestion and add a checklist item. The 'do not add a checklist item' button will take you to the second mandatory question.",
   answers: [
     {
-      buttonText: "Add checklist item",
+      buttonText: "Under 18",
       checklistItems: [
         {
           checkItemHeader: "Successful checklist item!",
@@ -17,40 +17,41 @@ export const firstQuestion = {
           },
         },
       ],
-      nextQuestion: {
-        //* NESTED SUB-QUESTION
-        questionHeader: "This a nested sub-question.",
-        questionSubHeader:
-          "Both items below should take you to the second mandatory question.",
-        answers: [
-          {
-            buttonText: "Add checklist item",
-            checklistItems: [
-              {
-                checkItemHeader: "Successful checklist item!",
-                checkItemBodyText:
-                  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea molestiae, ex quibusdam at nostrum dolorem laborum omnis non quaerat cumque placeat nulla eum consectetur a et dolore? Maxime, molestias at?",
-                checkItemButton: {
-                  text: "You can do anything...",
-                  url: "https://zombo.com/",
-                },
-              },
-            ],
-            nextQuestion: secondQuestion,
-            done: true,
-          },
-          {
-            buttonText: "Do NOT add a checklist item",
-            checklistItems: [],
-            nextQuestion: secondQuestion,
-            done: true,
-          },
-        ],
-      },
+      nextQuestion: secondQuestion,
+      // {
+      //   //* NESTED SUB-QUESTION
+      //   questionHeader: "This a nested sub-question.",
+      //   questionSubHeader:
+      //     "Both items below should take you to the second mandatory question.",
+      //   answers: [
+      //     {
+      //       buttonText: "Add checklist item",
+      //       checklistItems: [
+      //         {
+      //           checkItemHeader: "Successful checklist item!",
+      //           checkItemBodyText:
+      //             "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea molestiae, ex quibusdam at nostrum dolorem laborum omnis non quaerat cumque placeat nulla eum consectetur a et dolore? Maxime, molestias at?",
+      //           checkItemButton: {
+      //             text: "You can do anything...",
+      //             url: "https://zombo.com/",
+      //           },
+      //         },
+      //       ],
+      //       nextQuestion: secondQuestion,
+      //       done: true,
+      //     },
+      //     {
+      //       buttonText: "Do NOT add a checklist item",
+      //       checklistItems: [],
+      //       nextQuestion: secondQuestion,
+      //       done: true,
+      //     },
+      //   ],
+      // },
       done: true,
     },
     {
-      buttonText: "Do NOT add a checklist item",
+      buttonText: "18+",
       checklistItems: [],
       nextQuestion: secondQuestion,
       done: true,
